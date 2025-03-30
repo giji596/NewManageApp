b8e62f9337806671674ddc2452f1b878049e048a
Silver-Ginji <silver2ndginji@gmail.com>
2025-03-30 10:12:22 +0900

    ディレクトリ変更

--- my-app/src/component/CustomMenu/CustomMenuWrapper/CustomMenuWrapper.stories.tsx
+++ my-app/src/component/CustomMenu/CustomMenuWrapper/CustomMenuWrapper.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import CustomMenuWrapper from "./CustomMenuWrapper";
+import CustomMenuWrapper from "../CustomMenuWrapper";
 
 const meta = {
   component: CustomMenuWrapper,
