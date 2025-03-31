a2cc83ae9f680a1832a76f9f1f8dfaad9a7a8151
Silver-Ginji <silver2ndginji@gmail.com>
2025-03-31 15:52:41 +0900

    ui:デザイン変更

--- my-app/src/component/menu/content/CustomMenuCheckBox/CustomMenuCheckBox.tsx
+++ my-app/src/component/menu/content/CustomMenuCheckBox/CustomMenuCheckBox.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, FormControlLabel, MenuItem } from "@mui/material";
+import { MenuItem } from "@mui/material";
 
 type Props = {
   /** 選択肢の配列 */
@@ -16,7 +16,34 @@ export default function CustomMenuCheckBox({
 }: Props) {
   return Object.entries(checkList).map(([name, bool]) => (
     <MenuItem key={name} onClick={() => onClickSelect(name)}>
-      <FormControlLabel control={<Checkbox checked={bool} />} label={name} />
+      {name}
+      <div
+        style={{
+          width: 12,
+          height: 12,
+          border: "1px solid rgba(26, 26, 26, 0.14)",
+          borderRadius: "50%", // 丸くする
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginLeft: 8,
+          marginRight: 8,
+          backgroundColor: "transparent", // 選択された場合の背景色
+          transition: "all 0.3s ease", // アニメーションのスムーズさ
+          boxShadow: bool ? "0 0 10px rgba(25, 118, 210, 0.6)" : "none", // 選択時にグラデーションっぽく外側に広がる影
+        }}
+      >
+        {bool && (
+          <div
+            style={{
+              width: 10,
+              height: 10,
+              borderRadius: "50%", // 中も丸く
+              backgroundColor: "rgb(28, 164, 255)", // チェック部分の色
+            }}
+          />
+        )}
+      </div>
     </MenuItem>
   ));
 }
