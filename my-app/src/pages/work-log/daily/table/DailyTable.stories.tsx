f16eab913774e9eba7f8a929f1f608be780fdc2c
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-02 11:48:46 +0900

    feature:ロード状態とからの時の状態を作成

--- my-app/src/pages/work-log/daily/table/DailyTable.stories.tsx
+++ my-app/src/pages/work-log/daily/table/DailyTable.stories.tsx
@@ -5,13 +5,17 @@ import { DUMMY_DAILY_SUMMARY_DATA } from "@/dummy/daily-page";
 
 const meta = {
   component: DailyTable,
-  args: { itemList: DUMMY_DAILY_SUMMARY_DATA },
+  args: {
+    itemList: DUMMY_DAILY_SUMMARY_DATA,
+    isLoading: false,
+    onClickRow: () => {},
+  },
 } satisfies Meta<typeof DailyTable>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {},
-};
+export const Default: Story = {};
+export const Loading: Story = { args: { isLoading: true } };
+export const Empty: Story = { args: { itemList: [] } };
