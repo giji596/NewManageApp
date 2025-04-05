4af8f6bffd7baa4bd7c50900539af0207c65c27a
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-05 11:12:36 +0900

    ui:日付詳細ページのタスクリストのUI

---
+++ my-app/src/pages/work-log/daily/:id/task-list/TaskList.tsx
@@ -0,0 +1,44 @@
+import { Stack } from "@mui/material";
+import TaskTable from "./table/TaskTable";
+import TaskMenu from "./task-menu/TaskMenu";
+import { DailyDetailTaskTableType } from "@/type/Task";
+
+type Props = {
+  /** タスクの一覧 */
+  taskList: DailyDetailTaskTableType[];
+  /** ロード中かどうか */
+  isLoading: boolean;
+  /** タスクページへ移動する関数 */
+  navigateTaskPage: (id: number) => void;
+  /** カテゴリページへ移動する関数 */
+  navigateCategoryPage: (id: number) => void;
+};
+
+/**
+ * 日付詳細ページのタスク表示部分
+ */
+export default function TaskList({
+  taskList,
+  isLoading,
+  navigateTaskPage,
+  navigateCategoryPage,
+}: Props) {
+  return (
+    <>
+      <Stack>
+        <TaskMenu
+          isActive={true}
+          onClickEdit={() => {}} // TODO:ダイアログ作ったら修正
+          onClickNavigateTask={() => navigateTaskPage(0)} // TODO:後でパラメータ修正
+          onClickNavigateCategory={() => navigateCategoryPage(0)} // TODO:後でパラメータ修正
+        />
+        <TaskTable
+          taskList={taskList}
+          isLoading={isLoading}
+          onClickRow={() => {}} // TODO:ろじっくつくるとき
+        />
+      </Stack>
+      {/** TODO:　ここに編集用のダイアログ */}
+    </>
+  );
+}
