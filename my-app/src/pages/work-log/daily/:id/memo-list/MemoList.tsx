9bbf442acf7128d274203ac6f904af8d3f7f0ae8
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-06 11:40:44 +0900

    feature:機能面の実装

--- my-app/src/pages/work-log/daily/:id/memo-list/MemoList.tsx
+++ my-app/src/pages/work-log/daily/:id/memo-list/MemoList.tsx
@@ -4,6 +4,10 @@ import { MemoDailyTask } from "@/type/Memo";
 import CustomTableBody from "./table-body/CustomTableBody";
 import TableBodyLoading from "@/component/table/body/TableBodyLoading/TableBodyLoading";
 import TableBodyNoItem from "@/component/table/body/TableBodyNoItem/TableBodyNoItem";
+import CustomMenuWrapper from "@/component/menu/CustomMenuWrapper/CustomMenuWrapper";
+import CustomMenuCheckBox from "@/component/menu/content/CustomMenuCheckBox/CustomMenuCheckBox";
+import CustomMenuWrapperLogic from "@/component/menu/CustomMenuWrapper/CustomMenuWrapperLogic";
+import MemoListLogic from "./MemoListLogic";
 
 type Props = {
   /** メモのアイテム一覧 */
@@ -13,33 +17,60 @@ type Props = {
 };
 
 export default function MemoList({ memoItemList, isLoading }: Props) {
+  const { handleMouseEnter, handleMouseLeave, ...prev } =
+    CustomMenuWrapperLogic();
+  const {
+    isActiveRow,
+    handleClickRow,
+    isAsc,
+    isSelectedTitle,
+    handleSetSortTarget,
+    doSortByTitle,
+    taskFilterList,
+    toggleTaskFilterCheckBox,
+    doFilterByFilterList,
+  } = MemoListLogic({ memoItemList });
   return (
-    <TableContainer>
-      <Table sx={{ tableLayout: "fixed " }}>
-        <CustomTableHeader
-          isAsc={false}
-          isSelected={() => false}
-          onClickTitle={() => {}}
-          onHoverTitle={() => {}}
-          onLeaveHoverTitle={() => {}}
+    <>
+      <TableContainer>
+        <Table sx={{ tableLayout: "fixed " }}>
+          <CustomTableHeader
+            isAsc={isAsc}
+            isSelected={isSelectedTitle}
+            onClickTitle={handleSetSortTarget}
+            onHoverTitle={handleMouseEnter}
+            onLeaveHoverTitle={handleMouseLeave}
+          />
+          <TableBody>
+            {isLoading && <TableBodyLoading colCount={2} />}
+            {!isLoading && memoItemList.length === 0 && (
+              <TableBodyNoItem colCount={2} />
+            )}
+            {!isLoading &&
+              memoItemList.length !== 0 &&
+              memoItemList
+                .sort(doSortByTitle)
+                .filter(doFilterByFilterList)
+                .map((item) => (
+                  <CustomTableBody
+                    key={item.id}
+                    memoItem={item}
+                    isActive={isActiveRow(item.id)}
+                    onClickRow={handleClickRow}
+                  />
+                ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {/** タスク名にホバー時に表示するメニュー(フィルター関連) */}
+      <CustomMenuWrapper
+        logic={{ handleMouseEnter, handleMouseLeave, ...prev }}
+      >
+        <CustomMenuCheckBox
+          checkList={taskFilterList}
+          onClickSelect={toggleTaskFilterCheckBox}
         />
-        <TableBody>
-          {isLoading && <TableBodyLoading colCount={2} />}
-          {!isLoading && memoItemList.length === 0 && (
-            <TableBodyNoItem colCount={2} />
-          )}
-          {!isLoading &&
-            memoItemList.length !== 0 &&
-            memoItemList.map((item) => (
-              <CustomTableBody
-                key={item.id}
-                memoItem={item}
-                isActive={false}
-                onClickRow={() => {}}
-              />
-            ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+      </CustomMenuWrapper>
+    </>
   );
 }
---
+++ my-app/src/pages/work-log/daily/:id/memo-list/MemoListLogic.ts
@@ -0,0 +1,139 @@
+import { MemoDailyTask } from "@/type/Memo";
+import { useCallback, useState } from "react";
+
+type Props = {
+  /** メモの一覧 */
+  memoItemList: MemoDailyTask[];
+};
+
+/**
+ * 日付詳細 - メモリストのロジック部分
+ */
+export default function MemoListLogic({ memoItemList }: Props) {
+  // itemリストに存在するタスク一覧
+  const defaultTaskFilterList = memoItemList.reduce(
+    (a: Record<string, boolean>, b) => {
+      const taskName = b.task.name;
+      if (!(taskName in a)) {
+        a[taskName] = false;
+      }
+      return a;
+    },
+    {}
+  );
+
+  const [selectedHeaderTitle, setSelectedHeaderTitle] = useState<string | null>(
+    null
+  );
+  const [isAsc, setIsAsc] = useState<boolean>(true);
+  const [selectedRowId, setSelectedRowId] = useState<number | null>(null);
+  const [taskFilterList, setTaskFilterList] = useState<Record<string, boolean>>(
+    defaultTaskFilterList
+  );
+
+  const isActiveRow = useCallback(
+    (id: number) => selectedRowId === id,
+    [selectedRowId]
+  );
+
+  const handleClickRow = useCallback(
+    (id: number) => {
+      if (selectedRowId === id) {
+        setSelectedRowId(null);
+      } else {
+        setSelectedRowId(id);
+      }
+    },
+    [selectedRowId]
+  );
+
+  const isSelectedTitle = useCallback(
+    (title: string) => selectedHeaderTitle === title,
+    [selectedHeaderTitle]
+  );
+
+  // プロパティをソート設定する関数
+  const handleSetSortTarget = useCallback(
+    (title: string): void => {
+      // 選択中の場合:ascとdescを入れ替える
+      if (isSelectedTitle(title)) {
+        setIsAsc(!isAsc);
+      } else {
+        setSelectedHeaderTitle(title);
+        setIsAsc(true);
+      }
+    },
+    [isAsc, isSelectedTitle]
+  );
+  // タスクフィルターリストのチェックのOnOffを切り替える関数
+  const toggleTaskFilterCheckBox = useCallback(
+    (name: string) => {
+      const newValue = !taskFilterList[name];
+      setTaskFilterList((prev) => ({ ...prev, [name]: newValue }));
+    },
+    [, taskFilterList]
+  );
+
+  // ソート関数
+  const doSortByTitle = useCallback(
+    (a: MemoDailyTask, b: MemoDailyTask) => {
+      switch (selectedHeaderTitle) {
+        case "タイトル":
+          return isAsc
+            ? a.title.localeCompare(b.title)
+            : b.title.localeCompare(a.title);
+        case "タスク名":
+          return isAsc
+            ? a.task.name.localeCompare(b.task.name)
+            : b.task.name.localeCompare(a.task.name);
+        default:
+          return 0;
+      }
+    },
+    [isAsc, selectedHeaderTitle]
+  );
+
+  // 選択されている内容に応じてフィルターする関数
+  const doFilterByFilterList = useCallback(
+    (item: MemoDailyTask): boolean => {
+      // フィルターがセットされていない場合、trueを返してフィルターしない
+      const isNoTaskFilter = Object.values(taskFilterList).every(
+        (value) => value === false
+      );
+      if (isNoTaskFilter) {
+        return true;
+      }
+      // フィルターが存在する場合にカット対象か検証して、対象であれば早期にfalseでreturnする
+      const isCutByTask = !taskFilterList[item.task.name];
+      if (isCutByTask) {
+        return false;
+      }
+      return true;
+    },
+    [taskFilterList]
+  );
+
+  return {
+    /** 指定されたidの列がアクティブかどうかを求める */
+    isActiveRow,
+    /** 行をクリックした際のハンドラー
+     * 指定した行をActiveにする
+     * すでにActiveなら非Activeにする
+     */
+    handleClickRow,
+    /** ソートが昇順か降順か */
+    isAsc,
+    /** ソート対象に選択されているかどうかを調べる */
+    isSelectedTitle,
+    /** ソート対象に選択するハンドラー */
+    handleSetSortTarget,
+    /** ソートする関数 */
+    doSortByTitle,
+    /** タスクのフィルター対象の一覧(key:value=string:booleanのオブジェクト) */
+    taskFilterList,
+    /** タスクのフィルターリストのチェックボックスを切り替える関数 */
+    toggleTaskFilterCheckBox,
+    /** フィルターリストに応じてフィルターする関数 */
+    doFilterByFilterList,
+  };
+}
