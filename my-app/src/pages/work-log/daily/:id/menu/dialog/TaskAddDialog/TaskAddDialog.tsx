3ccb0f53a3bb0970f1689ad584d848c4280ebe4e
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-08 10:20:26 +0900

    とりまフラグメントで囲う

--- my-app/src/pages/work-log/daily/:id/menu/dialog/TaskAddDialog/TaskAddDialog.tsx
+++ my-app/src/pages/work-log/daily/:id/menu/dialog/TaskAddDialog/TaskAddDialog.tsx
@@ -37,83 +37,85 @@ export default function TaskAddDialog({ open, onClose }: Props) {
     handleAddDailyTask,
   } = TaskAddDialogLogic();
   return (
-    <Dialog open={open} onClose={onClose} fullWidth>
-      <DialogTitle>タスクを追加する</DialogTitle>
-      {/**コンテンツの部分 */}
-      <Stack px={2} spacing={1}>
-        {/** カテゴリのところ */}
-        <Stack direction="row" spacing={1}>
-          {/** セレクト*/}
-          <FormControl fullWidth>
-            <InputLabel>カテゴリ名</InputLabel>
-            <Select
-              label="カテゴリ名"
-              value={String(selectedCategoryId)}
-              onChange={onChangeSelectedCategory}
-            >
-              {categoryList.map((category) => (
-                <MenuItem key={category.id} value={category.id}>
-                  {category.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          {/** 追加ボタン */}
-          <IconButton sx={{ width: 50, height: 50 }}>
-            <AddCircleOutlineIcon />
-          </IconButton>
-        </Stack>
-        {/** タスクのところ */}
-        <Stack direction="row" spacing={1}>
-          {/** セレクト */}
-          <FormControl fullWidth>
-            <InputLabel>タスク名</InputLabel>
-            {isLoading && (
-              <Select disabled label="タスク名" value={0}>
-                <MenuItem value={0}>
-                  <CircularProgress size={22} />
-                </MenuItem>
-              </Select>
-            )}
-            {!isLoading && taskList.length === 0 && (
-              <Select disabled label="タスク名" value={0}>
-                <MenuItem value={0}>タスクがありません</MenuItem>
-              </Select>
-            )}
-            {!isLoading && taskList.length > 0 && (
+    <>
+      <Dialog open={open} onClose={onClose} fullWidth>
+        <DialogTitle>タスクを追加する</DialogTitle>
+        {/**コンテンツの部分 */}
+        <Stack px={2} spacing={1}>
+          {/** カテゴリのところ */}
+          <Stack direction="row" spacing={1}>
+            {/** セレクト*/}
+            <FormControl fullWidth>
+              <InputLabel>カテゴリ名</InputLabel>
               <Select
-                label="タスク名"
-                value={String(selectedTaskId)}
-                onChange={onChangeSelectedTask}
+                label="カテゴリ名"
+                value={String(selectedCategoryId)}
+                onChange={onChangeSelectedCategory}
               >
-                {taskList.map((task) => (
-                  <MenuItem key={task.id} value={task.id}>
-                    {task.name}
+                {categoryList.map((category) => (
+                  <MenuItem key={category.id} value={category.id}>
+                    {category.name}
                   </MenuItem>
                 ))}
               </Select>
-            )}
-          </FormControl>
-          {/** アイコンボタン */}
-          <IconButton sx={{ width: 50, height: 50 }}>
-            <AddCircleOutlineIcon />
-          </IconButton>
+            </FormControl>
+            {/** 追加ボタン */}
+            <IconButton sx={{ width: 50, height: 50 }}>
+              <AddCircleOutlineIcon />
+            </IconButton>
+          </Stack>
+          {/** タスクのところ */}
+          <Stack direction="row" spacing={1}>
+            {/** セレクト */}
+            <FormControl fullWidth>
+              <InputLabel>タスク名</InputLabel>
+              {isLoading && (
+                <Select disabled label="タスク名" value={0}>
+                  <MenuItem value={0}>
+                    <CircularProgress size={22} />
+                  </MenuItem>
+                </Select>
+              )}
+              {!isLoading && taskList.length === 0 && (
+                <Select disabled label="タスク名" value={0}>
+                  <MenuItem value={0}>タスクがありません</MenuItem>
+                </Select>
+              )}
+              {!isLoading && taskList.length > 0 && (
+                <Select
+                  label="タスク名"
+                  value={String(selectedTaskId)}
+                  onChange={onChangeSelectedTask}
+                >
+                  {taskList.map((task) => (
+                    <MenuItem key={task.id} value={task.id}>
+                      {task.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              )}
+            </FormControl>
+            {/** アイコンボタン */}
+            <IconButton sx={{ width: 50, height: 50 }}>
+              <AddCircleOutlineIcon />
+            </IconButton>
+          </Stack>
         </Stack>
-      </Stack>
-      {/** ボタン */}
-      <DialogActions>
-        <Button color="error" onClick={onClose}>
-          キャンセル
-        </Button>
-        <Button
-          disabled={isLoading}
-          variant="contained"
-          startIcon={<AddTaskIcon />}
-          onClick={handleAddDailyTask}
-        >
-          追加
-        </Button>
-      </DialogActions>
-    </Dialog>
+        {/** ボタン */}
+        <DialogActions>
+          <Button color="error" onClick={onClose}>
+            キャンセル
+          </Button>
+          <Button
+            disabled={isLoading}
+            variant="contained"
+            startIcon={<AddTaskIcon />}
+            onClick={handleAddDailyTask}
+          >
+            追加
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
