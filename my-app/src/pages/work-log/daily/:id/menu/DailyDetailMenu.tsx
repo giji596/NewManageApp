aab4eaceb788a2c8291cd37825ebf7ff230e654d
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-08 10:41:46 +0900

    ダイアログ入れるのでふらぐめんとで囲う

--- my-app/src/pages/work-log/daily/:id/menu/DailyDetailMenu.tsx
+++ my-app/src/pages/work-log/daily/:id/menu/DailyDetailMenu.tsx
@@ -7,59 +7,61 @@ import AddCommentIcon from "@mui/icons-material/AddComment";
  */
 export default function DailyDetailMenu() {
   return (
-    <Stack spacing={2}>
-      {/** 日付と時間 */}
-      <Stack spacing={1}>
-        {/** TODO:ひづけを受け取る */}
-        <Typography>(ひづけ)</Typography>
-        <Stack direction="row">
-          {/** TODO:時間を受け取る */}
-          <Typography>稼働時間： X(h)</Typography>
-          <Stack
-            direction="row-reverse"
-            sx={{
-              width: "80%",
-              height: 20,
-              background:
-                "linear-gradient(to right,rgb(148, 244, 236),rgb(114, 255, 173),rgb(255, 53, 53))",
-              borderRadius: 1,
-            }}
-          >
+    <>
+      <Stack spacing={2}>
+        {/** 日付と時間 */}
+        <Stack spacing={1}>
+          {/** TODO:ひづけを受け取る */}
+          <Typography>(ひづけ)</Typography>
+          <Stack direction="row">
+            {/** TODO:時間を受け取る */}
+            <Typography>稼働時間： X(h)</Typography>
             <Stack
+              direction="row-reverse"
               sx={{
-                height: "100%",
-                width: "0%",
-                backgroundColor: "#eee",
-                animation: "grow 1s ease-out forwards",
-                "@keyframes grow": {
-                  "0%": { width: "100%" },
-                  "100%": {
-                    width: `${20}%`, // TODO:稼働時間から計算して渡す (10-稼働時間)/10*100
-                  },
-                },
+                width: "80%",
+                height: 20,
+                background:
+                  "linear-gradient(to right,rgb(148, 244, 236),rgb(114, 255, 173),rgb(255, 53, 53))",
+                borderRadius: 1,
               }}
-            />
+            >
+              <Stack
+                sx={{
+                  height: "100%",
+                  width: "0%",
+                  backgroundColor: "#eee",
+                  animation: "grow 1s ease-out forwards",
+                  "@keyframes grow": {
+                    "0%": { width: "100%" },
+                    "100%": {
+                      width: `${20}%`, // TODO:稼働時間から計算して渡す (10-稼働時間)/10*100
+                    },
+                  },
+                }}
+              />
+            </Stack>
           </Stack>
         </Stack>
+        {/** ナビゲーションボタン */}
+        <Stack p={2} spacing={0.5}>
+          {/** TODO:クリック時のイベントつけーる */}
+          <Button
+            variant="outlined"
+            startIcon={<AddTaskIcon />}
+            sx={{ width: "50%" }}
+          >
+            タスクを追加する
+          </Button>
+          <Button
+            variant="outlined"
+            startIcon={<AddCommentIcon />}
+            sx={{ width: "50%" }}
+          >
+            メモを追加する
+          </Button>
+        </Stack>
       </Stack>
-      {/** ナビゲーションボタン */}
-      <Stack p={2} spacing={0.5}>
-        {/** TODO:クリック時のイベントつけーる */}
-        <Button
-          variant="outlined"
-          startIcon={<AddTaskIcon />}
-          sx={{ width: "50%" }}
-        >
-          タスクを追加する
-        </Button>
-        <Button
-          variant="outlined"
-          startIcon={<AddCommentIcon />}
-          sx={{ width: "50%" }}
-        >
-          メモを追加する
-        </Button>
-      </Stack>
-    </Stack>
+    </>
   );
 }
