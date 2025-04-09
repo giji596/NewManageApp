a9dffa8f76d2d55ce4e2440fd5d2ac3cd476c238
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-09 17:55:52 +0900

    feature:お気に入り時のtableのラベル

---
+++ my-app/src/component/table/header/HeaderFavoriteLabel/HeaderFavoriteLabel.tsx
@@ -0,0 +1,24 @@
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import StarIcon from "@mui/icons-material/Star";
+import { Checkbox } from "@mui/material";
+
+type Props = {
+  /** チェック状態 */
+  isChecked: boolean;
+  /** クリック時のハンドラー */
+  onClick: () => void;
+};
+
+/**
+ * お気に入り用のチェックボックスラベル
+ */
+export default function HeaderFavoriteLabel({ isChecked, onClick }: Props) {
+  return (
+    <Checkbox
+      checked={isChecked}
+      onClick={onClick}
+      icon={<StarBorderIcon />}
+      checkedIcon={<StarIcon />}
+    />
+  );
+}
