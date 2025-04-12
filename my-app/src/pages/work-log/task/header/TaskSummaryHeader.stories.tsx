a2e418a2479230a5ccddf93493aa37295da8f4c9
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-12 13:16:50 +0900

    feature:タスク一覧ページヘッダー作成

---
+++ my-app/src/pages/work-log/task/header/TaskSummaryHeader.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import TaskSummaryHeader from "./TaskSummaryHeader";
+
+const meta = {
+  component: TaskSummaryHeader,
+  args: {
+    isDirty: false,
+    isSelected: false,
+    onClickSave: () => {},
+    onClickReset: () => {},
+    onClickNavigateDetail: () => {},
+  },
+} satisfies Meta<typeof TaskSummaryHeader>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+export const Selected: Story = { args: { isSelected: true } };
+export const Dirty: Story = { args: { isDirty: true } };
