3737d0279631195116d1df18501b3fca4d651ada
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-12 10:59:45 +0900

    fix:<form>を削除(HTMLのルール上、Table内にformは使えないので)

--- my-app/src/pages/work-log/task/table/body/TaskSummaryTableBody.tsx
+++ my-app/src/pages/work-log/task/table/body/TaskSummaryTableBody.tsx
@@ -32,52 +32,50 @@ export default function TaskSummaryTableBody({ taskItem }: Props) {
     taskItem,
   });
   return (
-    <form>
-      <TableRow sx={{ backgroundColor: backGroundColor }}>
-        {/** おきにいり(チェックボックス) */}
-        <TableCell>
+    <TableRow sx={{ backgroundColor: backGroundColor }}>
+      {/** おきにいり(チェックボックス) */}
+      <TableCell>
+        <Controller
+          name="isFavorite"
+          control={control}
+          render={({ field }) => (
+            <Checkbox
+              {...field}
+              icon={<StarBorderIcon />}
+              checkedIcon={<StarIcon />}
+            />
+          )}
+        />
+      </TableCell>
+      {/** タスク名(固定) */}
+      <TableCell>{taskItem.taskName}</TableCell>
+      {/** カテゴリ名(固定) */}
+      <TableCell>{taskItem.categoryName}</TableCell>
+      {/** 進捗{セレクト} */}
+      <TableCell>
+        <FormControl fullWidth>
+          <InputLabel>進捗</InputLabel>
           <Controller
-            name="isFavorite"
+            name="progress"
             control={control}
             render={({ field }) => (
-              <Checkbox
-                {...field}
-                icon={<StarBorderIcon />}
-                checkedIcon={<StarIcon />}
-              />
+              <Select {...field} label={"進捗"}>
+                {progressSelects.map((v) => (
+                  <MenuItem key={v} value={v}>
+                    {v}%
+                  </MenuItem>
+                ))}
+              </Select>
             )}
           />
-        </TableCell>
-        {/** タスク名(固定) */}
-        <TableCell>{taskItem.taskName}</TableCell>
-        {/** カテゴリ名(固定) */}
-        <TableCell>{taskItem.categoryName}</TableCell>
-        {/** 進捗{セレクト} */}
-        <TableCell>
-          <FormControl fullWidth>
-            <InputLabel>進捗</InputLabel>
-            <Controller
-              name="progress"
-              control={control}
-              render={({ field }) => (
-                <Select {...field} label={"進捗"}>
-                  {progressSelects.map((v) => (
-                    <MenuItem key={v} value={v}>
-                      {v}%
-                    </MenuItem>
-                  ))}
-                </Select>
-              )}
-            />
-          </FormControl>
-        </TableCell>
-        {/** 稼働合計(固定) */}
-        <TableCell>{taskItem.totalHours}</TableCell>
-        {/** 開始日(固定) */}
-        <TableCell>{startDateString}</TableCell>
-        {/** 最終稼働日(固定) */}
-        <TableCell>{lastDateString}</TableCell>
-      </TableRow>
-    </form>
+        </FormControl>
+      </TableCell>
+      {/** 稼働合計(固定) */}
+      <TableCell>{taskItem.totalHours}</TableCell>
+      {/** 開始日(固定) */}
+      <TableCell>{startDateString}</TableCell>
+      {/** 最終稼働日(固定) */}
+      <TableCell>{lastDateString}</TableCell>
+    </TableRow>
   );
 }
