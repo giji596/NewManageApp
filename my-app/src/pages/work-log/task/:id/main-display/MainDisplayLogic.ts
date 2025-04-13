f0fed3332b22b8b5b0e65b0eed426c018cfa866c
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-13 15:20:33 +0900

    feature:メイン表示部分のコンポーネント作成

---
+++ my-app/src/pages/work-log/task/:id/main-display/MainDisplayLogic.ts
@@ -0,0 +1,18 @@
+import { useMemo } from "react";
+
+type Props = {
+  /** 進捗 */
+  progress: number;
+};
+
+/**
+ * タスク詳細ページのメイン部分の表示のロジック
+ */
+export default function MainDisplayLogic({ progress }: Props) {
+  const progressGrayBarWidth = useMemo(() => 100 - progress, [progress]);
+
+  return {
+    /** 進行度のバーの灰色部分の長さ */
+    progressGrayBarWidth,
+  };
+}
