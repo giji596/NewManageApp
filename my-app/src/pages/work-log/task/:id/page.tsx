98afba09d3eaa626f690f25573100ee594542bad
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-13 16:57:37 +0900

    ui:ロード中の表示を追加

--- my-app/src/pages/work-log/task/:id/page.tsx
+++ my-app/src/pages/work-log/task/:id/page.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { CircularProgress, Stack } from "@mui/material";
 import MainDisplay from "./main-display/MainDisplay";
 import MemoList from "./memo-list/MemoList";
 import DateDisplay from "./date-display/DateDisplay";
@@ -10,6 +10,7 @@ import useTaskDetailPage from "./useTaskDetailPage";
  */
 export default function TaskDetailPage() {
   const {
+    isLoading,
     taskName,
     categoryName,
     isFavorite,
@@ -21,45 +22,57 @@ export default function TaskDetailPage() {
   } = useTaskDetailPage();
   return (
     <>
-      <Stack direction="row">
-        {/** TODO:isLoadingで分岐させて表示を変えさせる */}
-        {/**　左部分(メインディスプレイ/テーブル) */}
-        <Stack width="70%">
-          {/** メインディスプレイ */}
-          <Stack height={200} pt={2}>
-            <MainDisplay
-              taskName={taskName}
-              isFavorite={isFavorite}
-              categoryName={categoryName}
-              progress={progress}
-              totalHours={totalHours}
-              onClickNavigateCategoryPage={() => {}}
-            />
+      {isLoading && (
+        <Stack
+          width={1187}
+          height={616}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <CircularProgress />
+        </Stack>
+      )}
+      {!isLoading && (
+        <Stack direction="row">
+          {/** TODO:isLoadingで分岐させて表示を変えさせる */}
+          {/**　左部分(メインディスプレイ/テーブル) */}
+          <Stack width="70%">
+            {/** メインディスプレイ */}
+            <Stack height={200} pt={2}>
+              <MainDisplay
+                taskName={taskName}
+                isFavorite={isFavorite}
+                categoryName={categoryName}
+                progress={progress}
+                totalHours={totalHours}
+                onClickNavigateCategoryPage={() => {}}
+              />
+            </Stack>
+            {/** テーブル */}
+            <Stack height={400} overflow="auto">
+              <MemoList memoItemList={memoList} />
+            </Stack>
           </Stack>
-          {/** テーブル */}
-          <Stack height={400} overflow="auto">
-            <MemoList memoItemList={memoList} />
+          {/** 右部分(日付/アクションボタン) */}
+          <Stack width="30%" justifyContent={"space-between"}>
+            {/** 日付 */}
+            <Stack pt={3}>
+              <DateDisplay
+                startDate={startDateString}
+                lastDate={lastDateString}
+              />
+            </Stack>
+            {/** アクションボタン */}
+            <Stack>
+              <ActionButtons
+                onClickEdit={() => {}}
+                onClickComplete={() => {}}
+                onClickDelete={() => {}}
+              />
+            </Stack>
           </Stack>
         </Stack>
-        {/** 右部分(日付/アクションボタン) */}
-        <Stack width="30%" justifyContent={"space-between"}>
-          {/** 日付 */}
-          <Stack pt={3}>
-            <DateDisplay
-              startDate={startDateString}
-              lastDate={lastDateString}
-            />
-          </Stack>
-          {/** アクションボタン */}
-          <Stack>
-            <ActionButtons
-              onClickEdit={() => {}}
-              onClickComplete={() => {}}
-              onClickDelete={() => {}}
-            />
-          </Stack>
-        </Stack>
-      </Stack>
+      )}
     </>
   );
 }
