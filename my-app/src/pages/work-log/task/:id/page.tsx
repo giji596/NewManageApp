e0c5b9a0e1c6d6ec258b9ee5772d50a38abe12dd
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-13 16:23:40 +0900

    feature:ダミーのデータを表示

--- my-app/src/pages/work-log/task/:id/page.tsx
+++ my-app/src/pages/work-log/task/:id/page.tsx
@@ -3,36 +3,48 @@ import MainDisplay from "./main-display/MainDisplay";
 import MemoList from "./memo-list/MemoList";
 import DateDisplay from "./date-display/DateDisplay";
 import ActionButtons from "./action-buttons/ActionButtons";
+import useTaskDetailPage from "./useTaskDetailPage";
 
 /**
  * タスク詳細ページ
  */
 export default function TaskDetailPage() {
+  const {
+    taskName,
+    categoryName,
+    isFavorite,
+    progress,
+    totalHours,
+    startDateString,
+    lastDateString,
+    memoList,
+  } = useTaskDetailPage();
   return (
     <Stack direction="row">
+      {/** TODO:isLoadingで分岐させて表示を変えさせる */}
       {/**　左部分(メインディスプレイ/テーブル) */}
       <Stack width="70%">
         {/** メインディスプレイ */}
         <Stack height={200} pt={2}>
           <MainDisplay
-            taskName={""}
-            isFavorite={false}
-            categoryName={""}
-            progress={0}
-            totalHours={0}
+            taskName={taskName}
+            isFavorite={isFavorite}
+            categoryName={categoryName}
+            progress={progress}
+            totalHours={totalHours}
             onClickNavigateCategoryPage={() => {}}
           />
         </Stack>
         {/** テーブル */}
         <Stack height={400} overflow="auto">
-          <MemoList memoItemList={[]} />
+          <MemoList memoItemList={memoList} />
         </Stack>
       </Stack>
       {/** 右部分(日付/アクションボタン) */}
       <Stack width="30%" justifyContent={"space-between"}>
         {/** 日付 */}
         <Stack pt={3}>
-          <DateDisplay startDate={"2222/22/22"} lastDate={"4444/44/44"} />
+          <DateDisplay startDate={startDateString} lastDate={lastDateString} />
         </Stack>
         {/** アクションボタン */}
         <Stack>
