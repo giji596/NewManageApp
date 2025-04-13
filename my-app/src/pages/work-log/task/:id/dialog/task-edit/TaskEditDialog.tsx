a54cb77f05374ef397262af879056427a8d76780
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-13 18:25:25 +0900

    <form>でダイアログ内を囲んだ(RHF適応の前段階として)

--- my-app/src/pages/work-log/task/:id/dialog/task-edit/TaskEditDialog.tsx
+++ my-app/src/pages/work-log/task/:id/dialog/task-edit/TaskEditDialog.tsx
@@ -46,47 +46,49 @@ export default function TaskEditDialog({
   });
   return (
     <Dialog fullWidth open={open} onClose={onClose}>
-      {/** タイトル */}
-      <DialogTitle>タスクを編集</DialogTitle>
-      <Stack py={2} px={3.5} spacing={2}>
-        {/** タスク名 */}
-        <TextField
-          label={"タスク名"}
-          variant="standard"
-          sx={{ "& input": { pl: 1.5 } }}
-          slotProps={{ inputLabel: { sx: { pl: 3 } } }}
-        />
-        {/** カテゴリ */}
-        {isLoading && (
-          <Stack width="32px" height="32px" pl={2}>
-            <CircularProgress size="small" />
-          </Stack>
-        )}
-        {!isLoading && (
-          <FormControl fullWidth>
-            <InputLabel>カテゴリ名</InputLabel>
-            <Select
-              label="カテゴリ名"
-              variant="standard"
-              defaultValue={1}
-              sx={{ pl: 1.5 }}
-            >
-              {categoryList.map((item) => (
-                <MenuItem key={item.id} value={item.id}>
-                  {item.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        )}
-        {/** お気に入り */}
-        <FormControlLabel control={<Checkbox />} label="お気に入り" />
-      </Stack>
-      {/** ボタン */}
-      <DialogActions>
-        <Button color="error">キャンセル</Button>
-        <Button>保存</Button>
-      </DialogActions>
+      <form>
+        {/** タイトル */}
+        <DialogTitle>タスクを編集</DialogTitle>
+        <Stack py={2} px={3.5} spacing={2}>
+          {/** タスク名 */}
+          <TextField
+            label={"タスク名"}
+            variant="standard"
+            sx={{ "& input": { pl: 1.5 } }}
+            slotProps={{ inputLabel: { sx: { pl: 3 } } }}
+          />
+          {/** カテゴリ */}
+          {isLoading && (
+            <Stack width="32px" height="32px" pl={2}>
+              <CircularProgress size="small" />
+            </Stack>
+          )}
+          {!isLoading && (
+            <FormControl fullWidth>
+              <InputLabel>カテゴリ名</InputLabel>
+              <Select
+                label="カテゴリ名"
+                variant="standard"
+                defaultValue={1}
+                sx={{ pl: 1.5 }}
+              >
+                {categoryList.map((item) => (
+                  <MenuItem key={item.id} value={item.id}>
+                    {item.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          )}
+          {/** お気に入り */}
+          <FormControlLabel control={<Checkbox />} label="お気に入り" />
+        </Stack>
+        {/** ボタン */}
+        <DialogActions>
+          <Button color="error">キャンセル</Button>
+          <Button>保存</Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 }
