715376adb2a1abba2e7806b340979bf74e2ff500
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-13 14:50:02 +0900

    feature:日付の表示部分を作成

---
+++ my-app/src/pages/work-log/task/:id/date-display/DateDisplay.tsx
@@ -0,0 +1,26 @@
+import { Stack, Typography } from "@mui/material";
+
+type Props = {
+  /** 開始日 */
+  startDate: string;
+  /** 最終実施日 */
+  lastDate: string;
+};
+
+/**
+ * タスク詳細　日付の表示部分
+ */
+export default function DateDisplay({ startDate, lastDate }: Props) {
+  return (
+    <Stack spacing={2}>
+      <Stack alignItems={"center"}>
+        <Typography variant="subtitle1">開始日</Typography>
+        <Typography variant="h5">{startDate}</Typography>
+      </Stack>
+      <Stack alignItems={"center"}>
+        <Typography variant="subtitle1">最終実施日</Typography>
+        <Typography variant="h5">{lastDate}</Typography>
+      </Stack>
+    </Stack>
+  );
+}
