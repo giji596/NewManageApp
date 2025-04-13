845124c8adf09fcf2fb4350157de4534f4c5ed39
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-13 18:18:13 +0900

    とりあえずuseFormを呼び出し

--- my-app/src/pages/work-log/task/:id/dialog/task-edit/TaskEditDialogLogic.ts
+++ my-app/src/pages/work-log/task/:id/dialog/task-edit/TaskEditDialogLogic.ts
@@ -1,4 +1,6 @@
 import { CategoryOption } from "@/type/Category";
+import { useCallback } from "react";
+import { useForm } from "react-hook-form";
 
 type SubmitData = {
   /** タスク名 */
@@ -22,10 +24,28 @@ export default function TaskEditDialogLogic() {
   ];
   const isLoading = false;
 
+  const {
+    control,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<SubmitData>({
+    defaultValues: { taskName: "", categoryId: 1, isFavorite: false }, // TODO: 初期値が必要
+  });
+
+  const onSubmit = useCallback(async (data: SubmitData) => {
+    // TODO:ここで送信
+    console.log("送信でーた", data);
+  }, []);
   return {
     /** カテゴリの一覧 */
     categoryList,
     /* カテゴリ一覧のロード状態 */
     isLoading,
+    /** RHFのコントロールオブジェクト(MUIのコンポーネントに必須) */
+    control,
+    /** バリデーションの可否 */
+    isValid,
+    /** データ送信するハンドラー */
+    onSubmit: handleSubmit(onSubmit),
   };
 }
