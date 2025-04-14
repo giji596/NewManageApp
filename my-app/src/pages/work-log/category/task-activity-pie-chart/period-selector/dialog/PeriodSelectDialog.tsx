4c8e37c8962e270ea2f557fd3da9c6e840d6b357
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-14 11:55:57 +0900

    配置を調整　(Typographyはspacingで"〜"の配置を調整するために必要なので追加)

--- my-app/src/pages/work-log/category/task-activity-pie-chart/period-selector/dialog/PeriodSelectDialog.tsx
+++ my-app/src/pages/work-log/category/task-activity-pie-chart/period-selector/dialog/PeriodSelectDialog.tsx
@@ -8,6 +8,7 @@ import {
   MenuItem,
   Select,
   Stack,
+  Typography,
 } from "@mui/material";
 
 type Props = {
@@ -24,14 +25,14 @@ export default function PeriodSelectDialog({ open, onClose }: Props) {
   return (
     <Dialog fullWidth open={open} onClose={onClose}>
       <DialogTitle>期間を選択</DialogTitle>
-      <Stack direction="row">
+      <Stack direction="row" px={3} py={1.5} spacing={1} alignItems="center">
         <FormControl fullWidth>
           <InputLabel>開始期間</InputLabel>
           <Select label="開始期間">
             <MenuItem>開始期間</MenuItem>
           </Select>
         </FormControl>
-        〜
+        <Typography variant="h5">〜</Typography>
         <FormControl fullWidth>
           <InputLabel>終了期間</InputLabel>
           <Select label="終了期間">
