cb83ac2c1741ef9e7d5993413ebee60e4446ed11
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-14 09:20:15 +0900

    ストーリーブックのpramもれ

--- my-app/src/pages/work-log/task/:id/dialog/task-edit/TaskEditDialog.stories.tsx
+++ my-app/src/pages/work-log/task/:id/dialog/task-edit/TaskEditDialog.stories.tsx
@@ -1,18 +1,20 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import TaskEditDialog from './TaskEditDialog';
+import TaskEditDialog from "./TaskEditDialog";
 
 const meta = {
   component: TaskEditDialog,
+  args: {
+    open: true,
+    onClose: () => {},
+    initialTaskName: "タスク名",
+    initialCategoryId: 1,
+    initialIsFavorite: false,
+  },
 } satisfies Meta<typeof TaskEditDialog>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    open: true,
-    onClose: () => {}
-  }
-};
+export const Default: Story = {};
