d9128ca3729db60f151a046d13f2e2bb986a710b
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-14 15:02:15 +0900

    選択肢を取得するメソッドを実装

--- my-app/src/pages/work-log/category/task-activity-pie-chart/period-selector/dialog/select-menu/PeriodSelectMenuButtonLogic.ts
+++ my-app/src/pages/work-log/category/task-activity-pie-chart/period-selector/dialog/select-menu/PeriodSelectMenuButtonLogic.ts
@@ -1,3 +1,4 @@
+import { getDate, getDaysInMonth, getMonth, getYear } from "date-fns";
 import { useMemo, useState } from "react";
 
 type Props = {
@@ -34,6 +35,36 @@ export default function PeriodSelectMenuButtonLogic({
     return `${yearText}/${monthText}/${dayText}`;
   }, [day, month, year]);
 
+  const today = new Date();
+  const todayYear = getYear(today);
+  const todayMonth = getMonth(today) + 1;
+  const todayDay = getDate(today);
+  console.log(todayDay);
+
+  const yearSelect = useMemo(
+    () => Array.from({ length: 10 }, (_, i) => todayYear - i),
+    [todayYear]
+  );
+  const monthSelect = useMemo(() => {
+    if (year === todayYear) {
+      // 今年なら今月まで
+      return Array.from({ length: todayMonth }, (_, i) => i + 1);
+    } else {
+      // 今年以外なら1~12月
+      return Array.from({ length: 12 }, (_, i) => i + 1);
+    }
+  }, [todayMonth, todayYear, year]);
+  const daySelect = useMemo(() => {
+    // 今年の現在の月である場合は今日の日付まで
+    if (year === todayYear && month === todayMonth) {
+      return Array.from({ length: todayDay }, (_, i) => i + 1);
+    } else {
+      // それ以外なら選択中の年月の日数分
+      const dayCount = getDaysInMonth(new Date(year, month - 1));
+      return Array.from({ length: dayCount }, (_, i) => i + 1);
+    }
+  }, [month, todayDay, todayMonth, todayYear, year]);
+
   return {
     /** メニューの開く位置 */
     anchorEl,
@@ -45,5 +76,11 @@ export default function PeriodSelectMenuButtonLogic({
     handleClose,
     /** ボタンの表示テキスト */
     buttonText,
+    /** 年の選択賜 */
+    yearSelect,
+    /** 月の選択賜 */
+    monthSelect,
+    /** 日の選択賜 */
+    daySelect,
   };
 }
