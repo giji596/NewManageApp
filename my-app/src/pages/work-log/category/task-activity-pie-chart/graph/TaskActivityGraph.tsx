0152b51de879ac45ef87a1a57de97f1ed47d9037
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-15 10:53:51 +0900

    とりあえず基本のui部分を作成

---
+++ my-app/src/pages/work-log/category/task-activity-pie-chart/graph/TaskActivityGraph.tsx
@@ -0,0 +1,27 @@
+import { memo } from "react";
+import { Pie, PieChart, Tooltip } from "recharts";
+
+type Props = {
+  /** データ(仮) */
+  data: { name: string; value: number }[];
+};
+
+/**
+ * 特定の期間のタスク稼働を表現するグラフ
+ */
+const TaskActivityGraph = memo(function TaskActivityGraph({ data }: Props) {
+  return (
+    <PieChart width={200} height={200}>
+      <Pie
+        data={data}
+        dataKey="value"
+        cx="50%"
+        cy="50%"
+        outerRadius={90}
+        fill="#8884d8"
+      />
+      <Tooltip />
+    </PieChart>
+  );
+});
+export default TaskActivityGraph;
