49a21a81c1dbabcb170065131f2ceb1940d7468c
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-15 16:42:41 +0900

    こんぽーねんとに適応 あとstorybookのもれ

--- my-app/src/pages/work-log/category/task-activity-pie-chart/TaskActivityPieChart.stories.tsx
+++ my-app/src/pages/work-log/category/task-activity-pie-chart/TaskActivityPieChart.stories.tsx
@@ -1,15 +1,14 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import TaskActivityPieChart from './TaskActivityPieChart';
+import TaskActivityPieChart from "./TaskActivityPieChart";
 
 const meta = {
   component: TaskActivityPieChart,
+  args: { categoryId: 2 },
 } satisfies Meta<typeof TaskActivityPieChart>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {}
-};
+export const Default: Story = {};
