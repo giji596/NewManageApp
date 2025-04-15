0390d92c68d7f9682bccd339ef757f2130c4f53a
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-15 18:14:37 +0900

    そのまま表示するもの(タスク名,進捗率)をパラメータに追加

--- my-app/src/pages/work-log/category/category-task-list/table/body/CategoryTaskTableBody.stories.tsx
+++ my-app/src/pages/work-log/category/category-task-list/table/body/CategoryTaskTableBody.stories.tsx
@@ -1,15 +1,14 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import CategoryTaskTableBody from './CategoryTaskTableBody';
+import CategoryTaskTableBody from "./CategoryTaskTableBody";
 
 const meta = {
   component: CategoryTaskTableBody,
+  args: { taskName: "タスク1", progress: 80 },
 } satisfies Meta<typeof CategoryTaskTableBody>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {}
-};
+export const Default: Story = {};
--- my-app/src/pages/work-log/category/category-task-list/table/body/CategoryTaskTableBody.tsx
+++ my-app/src/pages/work-log/category/category-task-list/table/body/CategoryTaskTableBody.tsx
@@ -3,10 +3,20 @@ import { memo } from "react";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
 
+type Props = {
+  /** タスク名 */
+  taskName: string;
+  /** 進捗率 */
+  progress: number;
+};
+
 /**
  * カテゴリページのタスク一覧のテーブルボディ
  */
-const CategoryTaskTableBody = memo(function CategoryTaskTableBody() {
+const CategoryTaskTableBody = memo(function CategoryTaskTableBody({
+  taskName,
+  progress,
+}: Props) {
   return (
     <TableRow>
       {/** お気に入り */}
@@ -14,9 +24,9 @@ const CategoryTaskTableBody = memo(function CategoryTaskTableBody() {
         <StarBorderIcon />
       </TableCell>
       {/** タスク名 */}
-      <TableCell>タスク名</TableCell>
+      <TableCell>{taskName}</TableCell>
       {/** 進捗 */}
-      <TableCell>進捗%</TableCell>
+      <TableCell>{progress}%</TableCell>
       {/** 詳細へ移動するボタン */}
       <TableCell>
         <IconButton>
