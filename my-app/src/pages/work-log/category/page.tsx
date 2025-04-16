1ad1961095d3f9feeb6a7712b2039ffbdc452443
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-16 14:47:08 +0900

    ロジックを適応

--- my-app/src/pages/work-log/category/page.tsx
+++ my-app/src/pages/work-log/category/page.tsx
@@ -2,21 +2,26 @@ import { Stack } from "@mui/material";
 import CategorySelect from "./category-select/CategorySelect";
 import TaskActivityPieChart from "./task-activity-pie-chart/TaskActivityPieChart";
 import CategoryTaskList from "./category-task-list/CategoryTaskList";
+import CategoryPageLogic from "./logic";
 
 /**
  * カテゴリページ
  */
 export default function CategoryPage() {
+  const { selectedId, onChangeSelectedId } = CategoryPageLogic();
   return (
     <Stack direction="row" p={1.5}>
       {/** 左側(カテゴリ選択/期間グラフ) */}
       <Stack width="50%" justifyContent={"space-around"}>
-        <CategorySelect selectedCategoryId={1} onChangeCategoryId={() => {}} />
-        <TaskActivityPieChart categoryId={1} />
+        <CategorySelect
+          selectedCategoryId={selectedId}
+          onChangeCategoryId={onChangeSelectedId}
+        />
+        <TaskActivityPieChart categoryId={selectedId} />
       </Stack>
       {/** 右側(カテゴリ内タスクリスト) */}
       <Stack width="50%" height={500} pt={15}>
-        <CategoryTaskList categoryId={1} />
+        <CategoryTaskList categoryId={selectedId} />
       </Stack>
     </Stack>
   );
