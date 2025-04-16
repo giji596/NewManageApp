b4b8b16d509c1d52aee74deb004006993866e3ed
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-16 09:02:10 +0900

    ソート対象のパラメータを取得する関数を定義

--- my-app/src/pages/work-log/category/category-task-list/table/CategoryTaskTableLogic.ts
+++ my-app/src/pages/work-log/category/category-task-list/table/CategoryTaskTableLogic.ts
@@ -1,4 +1,7 @@
 import useTableSort from "@/hook/useTableSort";
+import { TableSortTargetType } from "@/type/Table";
+import { CategoryTaskList } from "@/type/Task";
+import { useCallback } from "react";
 
 /**
  * カテゴリページのタスク一覧のテーブルのロジック
@@ -6,4 +9,21 @@ import useTableSort from "@/hook/useTableSort";
 export default function CategoryTaskTableLogic() {
   const { target, isAsc, isSelected, handleClickSortLabel, doSort } =
     useTableSort({ initialTarget: "progress" });
+
+  const getSortTarget = useCallback(
+    (
+      a: CategoryTaskList,
+      b: CategoryTaskList
+    ): { a: TableSortTargetType; b: TableSortTargetType } => {
+      switch (target) {
+        case "タスク名":
+          return { a: a.name, b: b.name };
+        case "進捗":
+          return { a: a.progress, b: b.progress };
+        default:
+          return { a: a.id, b: b.id };
+      }
+    },
+    [target]
+  );
 }
