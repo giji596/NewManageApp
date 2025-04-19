204f86f00c251a1c5a43201ea4de346b96ef3b50
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-19 18:16:00 +0900

    mainのファイルをwork-log直下に移動してメインパスに設定

--- my-app/src/app/work-log/page.stories.tsx
+++ my-app/src/app/work-log/page.stories.tsx
@@ -1,6 +1,5 @@
-import type { Meta, StoryObj } from '@storybook/react';
-
-import Page from './page';
+import type { Meta, StoryObj } from "@storybook/react";
+import { Page } from "@/stories/Page";
 
 const meta = {
   component: Page,
@@ -11,5 +10,5 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {}
+  args: {},
 };
