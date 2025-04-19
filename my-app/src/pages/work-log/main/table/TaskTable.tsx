866f0a669933c69d4a94e30e6a284bdc2c3fcd3f
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-19 13:23:15 +0900

    ボディ部分に仮データを適応

--- my-app/src/pages/work-log/main/table/TaskTable.tsx
+++ my-app/src/pages/work-log/main/table/TaskTable.tsx
@@ -1,4 +1,5 @@
 import {
+  IconButton,
   Table,
   TableBody,
   TableCell,
@@ -7,11 +8,19 @@ import {
   TableRow,
 } from "@mui/material";
 import { memo } from "react";
+import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
 
 /**
  * メインページのタスクテーブルコンポーネント
  */
 const TaskTable = memo(function TaskTable() {
+  const data = [
+    { id: 1, name: "タスク1", progress: "80%" },
+    { id: 2, name: "タスク2", progress: "80%" },
+    { id: 3, name: "タスク3", progress: "70%" },
+    { id: 4, name: "タスク4", progress: "65%" },
+    { id: 5, name: "タスク5", progress: "20%" },
+  ];
   return (
     <TableContainer sx={{ width: 600, height: 400 }}>
       <Table stickyHeader sx={{ tableLayout: "fixed" }}>
@@ -28,14 +37,20 @@ const TaskTable = memo(function TaskTable() {
         </TableHead>
         {/** ボディ */}
         <TableBody>
-          <TableRow>
-            {/** タスク名 */}
-            <TableCell>タスク名</TableCell>
-            {/** 進捗 */}
-            <TableCell>進捗</TableCell>
-            {/** ボタン部分 */}
-            <TableCell>ボタン</TableCell>
-          </TableRow>
+          {data.map((item) => (
+            <TableRow key={item.id}>
+              {/** タスク名 */}
+              <TableCell>{item.name}</TableCell>
+              {/** 進捗 */}
+              <TableCell>{item.progress}</TableCell>
+              {/** ボタン部分 */}
+              <TableCell>
+                <IconButton>
+                  <DoubleArrowIcon />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </TableContainer>
