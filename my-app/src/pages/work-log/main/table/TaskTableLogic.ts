3ed7e0b0f29b8174de91a3258f97df91bfea2d30
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-19 13:30:39 +0900

    データの型定義をtypeファイルに追加

--- my-app/src/pages/work-log/main/table/TaskTableLogic.ts
+++ my-app/src/pages/work-log/main/table/TaskTableLogic.ts
@@ -1,9 +1,11 @@
+import { MainPageTaskTable } from "@/type/Task";
+
 /**
  * メインページのタスクテーブルコンポーネントのロジック
  */
 export default function TaskTableLogic() {
   // TODO:実際はデータフェッチさせる
-  const data = [
+  const data: MainPageTaskTable[] = [
     { id: 1, name: "タスク1", progress: "80%" },
     { id: 2, name: "タスク2", progress: "80%" },
     { id: 3, name: "タスク3", progress: "70%" },
