24ef73b4f8c7bc1aeb1ee1859a117947aee7aeca
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-19 10:02:26 +0900

    コンポーネントに適応

--- my-app/src/pages/work-log/main/pie-chart/MainPagePieChart.tsx
+++ my-app/src/pages/work-log/main/pie-chart/MainPagePieChart.tsx
@@ -1,11 +1,13 @@
 import { Stack, Typography } from "@mui/material";
 import { memo } from "react";
 import { Pie, PieChart, Tooltip } from "recharts";
+import MainPagePieChartLogic from "./MainPagePieChartLogic";
 
 /**
  * メインページの円グラフ
  */
 const MainPagePieChart = memo(function MainPagePieChart() {
+  const { data } = MainPagePieChartLogic();
   return (
     <Stack alignItems={"center"}>
       <Typography variant="h6" mb={2} color="text.secondary">
@@ -13,7 +15,7 @@ const MainPagePieChart = memo(function MainPagePieChart() {
       </Typography>
       <PieChart width={450} height={350}>
         <Pie
-          data={[{ name: "1", value: 1000 }]}
+          data={data}
           dataKey="value"
           cx="50%"
           cy="50%"
