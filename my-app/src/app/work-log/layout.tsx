be43b71ee4d0fc9d4b86e8b260f6a188e4ce01f0
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-20 12:11:45 +0900

    MUIのcssがSSRとCSRで異なるエラー(MUI Next.js 統合ページの手順に従って調整)

--- my-app/src/app/work-log/layout.tsx
+++ my-app/src/app/work-log/layout.tsx
@@ -1,13 +1,15 @@
 import Navbar from "@/component/Navbar/Navbar";
-
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 /**
  * work-logページの共通レイアウト
  */
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Navbar />
-      <main>{children}</main>
+      <AppRouterCacheProvider>
+        <Navbar />
+        <main>{children}</main>
+      </AppRouterCacheProvider>
     </>
   );
 }
