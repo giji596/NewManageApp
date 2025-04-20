e17fd395595d578f6f7eb803268863cb6fc359b8
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-20 14:28:59 +0900

    ナビゲーションの表示文言を変換する関数を追加して変換

--- my-app/src/component/Navbar/logic.ts
+++ my-app/src/component/Navbar/logic.ts
@@ -1,11 +1,36 @@
 import { usePathname } from "next/navigation";
 
+// 文言を変換する関数
+const exchangePathName = (nameList: string[], name: string, index: number) => {
+  switch (name) {
+    // 名称に応じて変換
+    case "work-log":
+      return "TOP";
+    case "daily":
+      return "日付(一覧)";
+    case "task":
+      return "タスク(一覧)";
+    case "category":
+      return "カテゴリ";
+    // これら以外の場合
+    default:
+      // [id]に当たる場合(Number化した時にNaNでない場合) ->一つ前の値がタスクかカテゴリかで分岐
+      if (isNaN(Number(name))) {
+        if (nameList[index - 1] === "daily") return "日付(詳細)";
+        if (nameList[index - 1] === "task") return "タスク(詳細)";
+      }
+      // 該当しなければ空白を返す
+      return "";
+  }
+};
+
 /**
  * NavBarコンポーネントのロジック部分
  */
 export const NavBarLogic = () => {
   const path = usePathname(); // ここでパス入手("/work-log/task"など)
-  const navPages = path.slice(1).split("/"); // 先頭の"/"を除いて配列化
+  const rawPages = path.slice(1).split("/"); // 先頭の"/"を除いて配列化
+  const navPages = rawPages.map((v, i) => exchangePathName(rawPages, v, i)); // 文言を変換
   const isLastPageIndex = (index: number): boolean =>
     navPages.length - 1 == index;
 
