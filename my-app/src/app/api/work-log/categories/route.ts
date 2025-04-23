83f89265a3116166b16e43852ab778e6414bdf1c
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-23 12:31:46 +0900

    重複するデータでrouteでエラー返せるように変更

--- my-app/src/app/api/work-log/categories/route.ts
+++ my-app/src/app/api/work-log/categories/route.ts
@@ -4,5 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   const body = (await req.json()) as { name: string };
   const data = await createCategory(body.name);
+  // null時(重複時) エラーを返す
+  if (data === null)
+    return NextResponse.json({ error: "duplicate error" }, { status: 400 });
+  // 正常時
   return NextResponse.json(data);
 }
