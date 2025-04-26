5e583e3d3c85d995f368aec42eaa6c4584685e14
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-26 18:28:14 +0900

    ハンドラー作成

---
+++ my-app/src/app/api/work-log/memos/[id]/body/route.ts
@@ -0,0 +1,12 @@
+import { getMemoBody } from "@/lib/services/memoService";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  _: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id: idParam } = await params;
+  const id = Number(idParam);
+  const res = await getMemoBody(id);
+  return NextResponse.json(res);
+}
