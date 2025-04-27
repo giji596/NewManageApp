c66bf6770b0d83def42c9f0f5b20c90503972872
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-27 14:48:59 +0900

    aspidaの型定義

--- my-app/src/aspida/api/work-log/tasks/index.ts
+++ my-app/src/aspida/api/work-log/tasks/index.ts
@@ -1,7 +1,11 @@
-import { TaskOption } from "@/type/Task";
+import { ReplaceDateWithString } from "@/type/common";
+import { TaskOption, TaskSummary } from "@/type/Task";
 import { DefineMethods } from "aspida";
 
 export type Methods = DefineMethods<{
+  get: {
+    resBody: ReplaceDateWithString<TaskSummary>[];
+  };
   post: {
     reqBody: { name: string; categoryId: number; isFavorite: boolean };
     resBody: TaskOption;
