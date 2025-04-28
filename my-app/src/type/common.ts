6fd285d244ebb826e17b153f5f148930db05cad6
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-28 12:15:27 +0900

    wip(ネストされたデータでエラーありなのでテスト不可) Dateをstringに変換する型定義を修正してネストしたデータにも対応(json()で返した値に一致するように)

--- my-app/src/type/common.ts
+++ my-app/src/type/common.ts
@@ -3,5 +3,9 @@
  * Tに元typeを入れて実装
  */
 export type ReplaceDateWithString<T> = {
-  [K in keyof T]: T[K] extends Date ? string : T[K];
+  [K in keyof T]: T[K] extends Date
+    ? string
+    : T[K] extends object
+    ? ReplaceDateWithString<T[K]> // ← 再帰で中も見に行く
+    : T[K];
 };
