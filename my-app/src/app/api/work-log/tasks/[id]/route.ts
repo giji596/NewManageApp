12f7855ee4a0e93111654455d9184ac6b4d5a868
Silver-Ginji <silver2ndginji@gmail.com>
2025-04-28 15:57:26 +0900

    ハンドラー作成

--- my-app/src/app/api/work-log/tasks/[id]/route.ts
+++ my-app/src/app/api/work-log/tasks/[id]/route.ts
@@ -1,4 +1,4 @@
-import { getTaskDetail } from "@/lib/services/taskService";
+import { getTaskDetail, updateTaskDetail } from "@/lib/services/taskService";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
@@ -11,5 +11,20 @@ export async function GET(
   if (res === null) {
     return NextResponse.json({ error: "Invalid Id" }, { status: 400 });
   }
+  return NextResponse.json(res);
+}
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id: idParam } = await params;
+  const id = Number(idParam);
+  const { taskName, categoryId, isFavorite } = (await req.json()) as {
+    taskName?: string;
+    categoryId?: number;
+    isFavorite?: boolean;
+  };
+  const res = updateTaskDetail(id, taskName, categoryId, isFavorite);
   return NextResponse.json(res);
 }
