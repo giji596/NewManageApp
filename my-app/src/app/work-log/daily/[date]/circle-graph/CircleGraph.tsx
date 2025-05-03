3faf1a2ea4b5313b29bcc855c1447612e1e5a790
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-03 14:08:43 +0900

    データのない時にNoDataPieGraph呼び出し

--- my-app/src/app/work-log/daily/[date]/circle-graph/CircleGraph.tsx
+++ my-app/src/app/work-log/daily/[date]/circle-graph/CircleGraph.tsx
@@ -4,6 +4,7 @@ import { DailyCategoryCircleGraph } from "@/type/Date";
 import CustomToolTipWrapper from "@/component/graph/tool-chip/CustomToolTipWrapper";
 import CustomToolTipContent from "@/component/graph/tool-chip/CustomToolTipContent";
 import CircleGraphLogic from "./CircleGraphLogic";
+import NoDataPieGraph from "@/component/graph/NoDataPie";
 
 type Props = {
   /** データ */
@@ -17,32 +18,35 @@ export default function CircleGraph({ data }: Props) {
   const { isNoData, getLabel } = CircleGraphLogic({ data });
   return (
     <>
-      <PieChart width={500} height={300}>
-        <Pie
-          data={data}
-          dataKey="value"
-          cx="50%"
-          cy="50%"
-          outerRadius={90}
-          fill={"#8884d8"}
-          label={getLabel}
-        />
-        <Tooltip
-          content={
-            <CustomToolTipWrapper>
-              {(dataItem: DailyCategoryCircleGraph) =>
-                dataItem.task.map((item) => (
-                  <CustomToolTipContent
-                    key={item.id}
-                    name={item.name}
-                    value={item.percent}
-                  />
-                ))
-              }
-            </CustomToolTipWrapper>
-          }
-        />
-      </PieChart>
+      {!isNoData && (
+        <PieChart width={500} height={300}>
+          <Pie
+            data={data}
+            dataKey="value"
+            cx="50%"
+            cy="50%"
+            outerRadius={90}
+            fill={"#8884d8"}
+            label={getLabel}
+          />
+          <Tooltip
+            content={
+              <CustomToolTipWrapper>
+                {(dataItem: DailyCategoryCircleGraph) =>
+                  dataItem.task.map((item) => (
+                    <CustomToolTipContent
+                      key={item.id}
+                      name={item.name}
+                      value={item.percent}
+                    />
+                  ))
+                }
+              </CustomToolTipWrapper>
+            }
+          />
+        </PieChart>
+      )}
+      {isNoData && <NoDataPieGraph width={500} height={300} radius={90} />}
     </>
   );
 }
