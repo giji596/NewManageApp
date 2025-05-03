789a642e905c67d8eb191a936f8c277fb00354f0
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-03 11:07:32 +0900

    クエリパラメータから現在のページのidを取得するように変更

--- my-app/src/app/work-log/category/logic.ts
+++ my-app/src/app/work-log/category/logic.ts
@@ -1,12 +1,13 @@
 import { SelectChangeEvent } from "@mui/material";
+import { useSearchParams } from "next/navigation";
 import { useCallback, useState } from "react";
 
 /**
  * カテゴリページのロジック
  */
 export default function CategoryPageLogic() {
-  // TODO:ここでidを取得 クエリから取得(クエリなければ1を受け取ればよさそう？)
-  const initialId = 1;
+  const searchParams = useSearchParams();
+  const initialId = Number(searchParams.get("id") ?? 1);
   const [selectedId, setSelectedId] = useState<number>(initialId);
   const onChangeSelectedId = useCallback((e: SelectChangeEvent) => {
     const newId = Number(e.target.value);
