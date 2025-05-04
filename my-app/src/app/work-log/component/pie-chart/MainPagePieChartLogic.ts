10e0f608f56371a24e7cccccd3298e25c6eba9c1
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-04 10:59:54 +0900

    FEに適応

--- my-app/src/app/work-log/component/pie-chart/MainPagePieChartLogic.ts
+++ my-app/src/app/work-log/component/pie-chart/MainPagePieChartLogic.ts
@@ -1,32 +1,17 @@
+import apiClient from "@/lib/apiClient";
+import useAspidaSWR from "@aspida/swr";
+
 /**
  * メインページの円グラフのロジック
  */
 export default function MainPagePieChartLogic() {
+  const { data: rawData } = useAspidaSWR(
+    apiClient.work_log.tasks.activities.last_month,
+    "get",
+    { key: "api/work-log/tasks/activities/last-month" }
+  );
   // TODO:でーたふぇっちする(整形された状態で)
-  const data = [
-    {
-      name: "カテゴリ1",
-      value: 500,
-      task: [
-        { name: "タスク1", hours: "30(h)" },
-        { name: "タスク2", hours: "12(h)" },
-        { name: "タスク3", hours: "8(h)" },
-      ],
-    },
-    {
-      name: "カテゴリ2",
-      value: 300,
-      task: [
-        { name: "タスク4", hours: "20(h)" },
-        { name: "タスク5", hours: "10(h)" },
-      ],
-    },
-    {
-      name: "カテゴリ3",
-      value: 200,
-      task: [{ name: "タスク6", hours: "20(h)" }],
-    },
-  ];
+  const data = rawData?.body ?? [];
 
   return {
     /** 表示するデータ */
