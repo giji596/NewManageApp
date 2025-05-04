792fc1cbb06d1df9102cb19b6820580f10154bad
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-04 16:49:19 +0900

    はんどらーていぎ

--- my-app/src/app/api/work-log/memos/tags/route.ts
+++ my-app/src/app/api/work-log/memos/tags/route.ts
@@ -1,7 +1,13 @@
-import { getMemoTags } from "@/lib/services/memoService";
-import { NextResponse } from "next/server";
+import { createTag, getMemoTags } from "@/lib/services/memoService";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   const data = await getMemoTags();
   return NextResponse.json(data);
+}
+
+export async function POST(req: NextRequest) {
+  const { tagName } = (await req.json()) as { tagName: string };
+  const res = await createTag(tagName);
+  return NextResponse.json(res);
 }
