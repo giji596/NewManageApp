cc3dd79d8fc276ecc9e28071265555f2dde2ffa1
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-05 13:13:52 +0900

    ダイアログ追加するのでフラグメントで全体囲う

--- my-app/src/app/work-log/daily/[date]/menu/dialog/MemoAddDialog/MemoAddDialog.tsx
+++ my-app/src/app/work-log/daily/[date]/menu/dialog/MemoAddDialog/MemoAddDialog.tsx
@@ -43,118 +43,130 @@ export default function MemoAddDialog({
     taskList,
   });
   return (
-    <Dialog open={open} onClose={onClose} fullWidth>
-      <DialogTitle>メモを追加</DialogTitle>
-      {/** タスクの選択フォーム */}
-      <form onSubmit={onSubmit}>
-        <Stack spacing={1} px={2}>
-          <FormControl fullWidth>
-            <InputLabel id="task-select-label">タスクを選ぶ</InputLabel>
-            {!isTaskSelected && (
-              <Controller
-                name="logId"
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <Select
-                    {...field}
-                    labelId="task-select-label"
-                    id="task-select"
-                    label="タスクを選ぶ"
-                  >
-                    {taskList.map((task) => (
-                      <MenuItem key={task.id} value={task.id}>
-                        {task.taskName}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                )}
-              />
-            )}
-            {isTaskSelected && (
-              <Select
-                labelId="task-select-label"
-                id="task-select-disabled"
-                disabled
-                label="タスクを選ぶ"
-                defaultValue={taskList[0].id}
-              >
-                <MenuItem value={taskList[0].id}>
-                  {taskList[0].taskName}
-                </MenuItem>
-              </Select>
-            )}
-          </FormControl>
-          {/** タイトル/タグ */}
-          <Stack direction="row" spacing={1}>
-            <Controller
-              name="title"
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => (
-                <TextField {...field} label="タイトル" sx={{ width: "70%" }} />
-              )}
-            />
-            {/** タグ */}
-            <Stack direction="row" width="30%">
-              {/** タグ選択賜 */}
-              <FormControl sx={{ width: "80%" }}>
-                <InputLabel id="tag-select-label">タグ</InputLabel>
+    <>
+      <Dialog open={open} onClose={onClose} fullWidth>
+        <DialogTitle>メモを追加</DialogTitle>
+        {/** タスクの選択フォーム */}
+        <form onSubmit={onSubmit}>
+          <Stack spacing={1} px={2}>
+            <FormControl fullWidth>
+              <InputLabel id="task-select-label">タスクを選ぶ</InputLabel>
+              {!isTaskSelected && (
                 <Controller
-                  name="tagId"
+                  name="logId"
                   control={control}
+                  rules={{ required: true }}
                   render={({ field }) => (
                     <Select
                       {...field}
-                      labelId="tag-select-label"
-                      id="tag-select-label"
-                      label="タグ"
+                      labelId="task-select-label"
+                      id="task-select"
+                      label="タスクを選ぶ"
                     >
-                      {tagList.map((tag) => (
-                        <MenuItem key={tag.id} value={tag.id}>
-                          {tag.name}
+                      {taskList.map((task) => (
+                        <MenuItem key={task.id} value={task.id}>
+                          {task.taskName}
                         </MenuItem>
                       ))}
                     </Select>
                   )}
                 />
-              </FormControl>
-              {/** タグ追加ボタン */}
-              <IconButton
-                sx={{
-                  width: 40,
-                  height: 40,
-                  alignSelf: "center",
-                }}
-              >
-                <AddCircleIcon />
-              </IconButton>
+              )}
+              {isTaskSelected && (
+                <Select
+                  labelId="task-select-label"
+                  id="task-select-disabled"
+                  disabled
+                  label="タスクを選ぶ"
+                  defaultValue={taskList[0].id}
+                >
+                  <MenuItem value={taskList[0].id}>
+                    {taskList[0].taskName}
+                  </MenuItem>
+                </Select>
+              )}
+            </FormControl>
+            {/** タイトル/タグ */}
+            <Stack direction="row" spacing={1}>
+              <Controller
+                name="title"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    label="タイトル"
+                    sx={{ width: "70%" }}
+                  />
+                )}
+              />
+              {/** タグ */}
+              <Stack direction="row" width="30%">
+                {/** タグ選択賜 */}
+                <FormControl sx={{ width: "80%" }}>
+                  <InputLabel id="tag-select-label">タグ</InputLabel>
+                  <Controller
+                    name="tagId"
+                    control={control}
+                    render={({ field }) => (
+                      <Select
+                        {...field}
+                        labelId="tag-select-label"
+                        id="tag-select-label"
+                        label="タグ"
+                      >
+                        {tagList.map((tag) => (
+                          <MenuItem key={tag.id} value={tag.id}>
+                            {tag.name}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    )}
+                  />
+                </FormControl>
+                {/** タグ追加ボタン */}
+                <IconButton
+                  sx={{
+                    width: 40,
+                    height: 40,
+                    alignSelf: "center",
+                  }}
+                >
+                  <AddCircleIcon />
+                </IconButton>
+              </Stack>
             </Stack>
+            {/**　本文 */}
+            <Controller
+              name="text"
+              control={control}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  id="text"
+                  label="本文"
+                  multiline
+                  rows={4}
+                />
+              )}
+            />
           </Stack>
-          {/**　本文 */}
-          <Controller
-            name="text"
-            control={control}
-            rules={{ required: true }}
-            render={({ field }) => (
-              <TextField {...field} id="text" label="本文" multiline rows={4} />
-            )}
-          />
-        </Stack>
-        <DialogActions>
-          <Button onClick={onClose} color="error">
-            キャンセル
-          </Button>
-          <Button
-            disabled={!isValid}
-            type="submit"
-            variant="contained"
-            startIcon={<NoteAddIcon />}
-          >
-            追加
-          </Button>
-        </DialogActions>
-      </form>
-    </Dialog>
+          <DialogActions>
+            <Button onClick={onClose} color="error">
+              キャンセル
+            </Button>
+            <Button
+              disabled={!isValid}
+              type="submit"
+              variant="contained"
+              startIcon={<NoteAddIcon />}
+            >
+              追加
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+    </>
   );
 }
