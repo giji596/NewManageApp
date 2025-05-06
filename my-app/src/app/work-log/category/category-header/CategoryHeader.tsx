f58cfac78a00d768499a3b5dda1e1571fd98df27
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-06 11:14:22 +0900

    パーツごとの配置を調整

--- my-app/src/app/work-log/category/category-header/CategoryHeader.tsx
+++ my-app/src/app/work-log/category/category-header/CategoryHeader.tsx
@@ -1,11 +1,15 @@
 "use client";
 import {
+  Button,
   FormControl,
+  FormLabel,
   MenuItem,
   Select,
   Stack,
   Typography,
 } from "@mui/material";
+import DoneIcon from "@mui/icons-material/Done";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import CategoryHeaderLogic from "./CategoryHeaderLogic";
 
 /**
@@ -16,28 +20,58 @@ export default function CategoryHeader() {
     CategoryHeaderLogic();
   return (
     <Stack direction="row" p={4} justifyContent={"space-between"}>
-      <Stack>カテゴリ関連の情報</Stack>
-      <Stack>
-        <Stack direction={"row"} alignItems={"center"}>
-          <Typography id="category-select-label">表示中のカテゴリ：</Typography>
-          <FormControl>
-            <Select
-              labelId="category-select-label"
-              id="category-select"
-              name="category-select"
-              variant="standard"
-              value={String(selectedCategoryId)}
-              onChange={onChangeCategoryId}
-            >
-              {categoryOptions.map((option) => (
-                <MenuItem key={option.id} value={option.id}>
-                  {option.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+      {/** 左部分(カテゴリ情報) */}
+      <Stack spacing={0.5}>
+        {/** カテゴリ名 + Completed? */}
+        <Stack direction="row" spacing={1}>
+          <Typography width="120px" textAlign={"end"} variant="h6">
+            カテゴリ名:
+          </Typography>
+          <Typography variant="h6">ここにカテゴリ名を入れる</Typography>
+          {/** TODO:完了かどうかで分岐 完了の文章 */}
+          <CheckCircleIcon color="success" />
+          <Typography color="success" variant="subtitle1" fontWeight={700}>
+            完了済み
+          </Typography>
         </Stack>
-        <div>完了ボタン的な</div>
+        {/** 合計稼働時間 */}
+        <Stack direction="row" spacing={1}>
+          <Typography width="120px" textAlign={"end"} variant="h6">
+            総稼働時間:
+          </Typography>
+          <Typography variant="h6">x(h)</Typography>
+          <Typography>[----グラフ?---]</Typography>
+        </Stack>
+        {/**  */}
+        <Stack direction="row" spacing={1}>
+          <Typography width="120px" textAlign={"end"} variant="h6">
+            稼働期間:
+          </Typography>
+          <Typography variant="h6">開始~最終更新日</Typography>
+        </Stack>
+      </Stack>
+      {/** 右部分(カテゴリ選択/完了ボタン) */}
+      <Stack spacing={1}>
+        <FormControl>
+          <FormLabel>カテゴリを選択</FormLabel>
+          <Select
+            labelId="category-select-label"
+            id="category-select"
+            name="category-select"
+            variant="standard"
+            value={String(selectedCategoryId)}
+            onChange={onChangeCategoryId}
+          >
+            {categoryOptions.map((option) => (
+              <MenuItem key={option.id} value={option.id}>
+                {option.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Button startIcon={<DoneIcon />} variant="outlined" color="success">
+          完了する
+        </Button>
       </Stack>
     </Stack>
   );
