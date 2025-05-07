09a5f1ce731650e05751fa077ed35f5571cfee56
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-07 15:05:24 +0900

    配置の間隔を調整

--- my-app/src/app/work-log/category/category-header/component/CategoryDisplayRangeDialog/CategoryDisplayRangeDialog.tsx
+++ my-app/src/app/work-log/category/category-header/component/CategoryDisplayRangeDialog/CategoryDisplayRangeDialog.tsx
@@ -20,7 +20,7 @@ const CategoryDisplayRangeDialog = memo(function CategoryDisplayRangeDialog() {
   return (
     <Dialog open={true /** TODO */}>
       <DialogTitle>期間を設定</DialogTitle>
-      <FormControl>
+      <FormControl sx={{ px: 3 }}>
         {/** メイン部分 */}
         {/** 期間ラジオグループ */}
         <RadioGroup row>
@@ -30,7 +30,13 @@ const CategoryDisplayRangeDialog = memo(function CategoryDisplayRangeDialog() {
         </RadioGroup>
       </FormControl>
       {/** 日付選択 */}
-      <Stack direction="row">
+      <Stack
+        direction="row"
+        py={2}
+        justifyContent={"center"}
+        alignItems={"center"}
+        spacing={2}
+      >
         <PeriodSelectMenuButton
           year={2025}
           month={5}
@@ -50,10 +56,10 @@ const CategoryDisplayRangeDialog = memo(function CategoryDisplayRangeDialog() {
         />
       </Stack>
       {/** 下部(チェックボックス + ボタン) */}
-      <Stack direction="row" justifyContent={"space-between"}>
+      <Stack direction="row" justifyContent={"space-between"} px={2} pb={2}>
         {/** 完了込みかのチェックボックス */}
         <FormControlLabel control={<Checkbox />} label="完了済みを除く" />
-        <Stack direction="row">
+        <Stack direction="row" spacing={1}>
           <Button color="error">キャンセル</Button>
           <Button>適応</Button>
         </Stack>
