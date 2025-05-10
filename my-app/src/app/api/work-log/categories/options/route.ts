42a88397c1cad9aedc9d881386527a4f2c1c4389
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-10 18:46:53 +0900

    はんどらー修正

--- my-app/src/app/api/work-log/categories/options/route.ts
+++ my-app/src/app/api/work-log/categories/options/route.ts
@@ -1,9 +1,17 @@
 import { getCategoryOptions } from "@/lib/services/categoryService";
-import { CategoryOption } from "@/type/Category";
-import { NextResponse } from "next/server";
+import { CategoryHeaderQuery, CategoryOption } from "@/type/Category";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(): Promise<NextResponse<CategoryOption[]>> {
-  const data = await getCategoryOptions();
+export async function GET(
+  req: NextRequest
+): Promise<NextResponse<CategoryOption[]>> {
+  // クエリ
+  const { searchParams } = new URL(req.url);
+  // 全てのクエリをオブジェクト化
+  const queryObj = Object.fromEntries(
+    searchParams.entries()
+  ) as CategoryHeaderQuery; // aspidaでクエリの型定義は合わせてるのでasで
+  const data = await getCategoryOptions(queryObj);
   if (data.length === 0)
     return NextResponse.json([{ id: 0, name: "カテゴリがありません" }]);
   return NextResponse.json(data);
