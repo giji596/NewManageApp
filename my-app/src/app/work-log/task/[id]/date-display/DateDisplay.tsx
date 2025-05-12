c47b1bfad59a2c078961fd698b14373e014eeb89
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-12 17:42:56 +0900

    繋ぎ込み

--- my-app/src/app/work-log/task/[id]/date-display/DateDisplay.tsx
+++ my-app/src/app/work-log/task/[id]/date-display/DateDisplay.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Stack, Typography } from "@mui/material";
+import WorkCalendarPopoverButton from "./work-calendar-menu-button/WorkCalendarPopoverButton";
 
 type Props = {
   /** 開始日 */
@@ -18,10 +19,11 @@ export default function DateDisplay({ startDate, lastDate }: Props) {
         <Typography variant="subtitle1">開始日</Typography>
         <Typography variant="h5">{startDate}</Typography>
       </Stack>
-      <Stack alignItems={"center"}>
+      <Stack alignItems={"center"} pb={2}>
         <Typography variant="subtitle1">最終実施日</Typography>
         <Typography variant="h5">{lastDate}</Typography>
       </Stack>
+      <WorkCalendarPopoverButton startDate={startDate} lastDate={lastDate} />
     </Stack>
   );
 }
