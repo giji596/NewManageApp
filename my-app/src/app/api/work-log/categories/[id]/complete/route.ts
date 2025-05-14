c138fc1073ba79bbd579b1c807b2705e1998703c
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-14 09:24:52 +0900

    ハンドラー定義

---
+++ my-app/src/app/api/work-log/categories/[id]/complete/route.ts
@@ -0,0 +1,13 @@
+import { updateCategoryCompleted } from "@/lib/services/categoryService";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  _: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  // パラメータ
+  const { id: idParam } = await params;
+  const id = Number(idParam);
+  const res = await updateCategoryCompleted(id);
+  return NextResponse.json(res);
+}
