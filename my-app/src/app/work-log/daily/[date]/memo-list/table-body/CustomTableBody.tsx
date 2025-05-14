8910760558dedb65fc570b84f5ade4ac59c09b79
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-14 14:58:38 +0900

    選択時のボディの表示を設定(薄青色にハイライト)

--- my-app/src/app/work-log/daily/[date]/memo-list/table-body/CustomTableBody.tsx
+++ my-app/src/app/work-log/daily/[date]/memo-list/table-body/CustomTableBody.tsx
@@ -4,6 +4,7 @@ import { TableRow, TableCell, Collapse, Box, IconButton } from "@mui/material";
 import AspectRatioIcon from "@mui/icons-material/AspectRatio";
 import useDialog from "@/hook/useDialog";
 import MemoEditDialog from "@/component/dialog/memo-edit-dialog/MemoEditDialog";
+import { CustomTableBodyLogic } from "./CustomTableBodyLogic";
 
 type Props = {
   /** メモ */
@@ -22,16 +23,21 @@ type Props = {
 export default function CustomTableBody({
   memoItem,
   isActive,
+  isHighlighted,
   onClickRow,
 }: Props) {
   const { open, onClose, onOpen } = useDialog();
+  const { backgroundColor } = CustomTableBodyLogic({ isHighlighted });
   return (
     <>
       <TableRow
         hover
         selected={isActive}
         onClick={() => onClickRow(memoItem.id)}
-        sx={{ "& > *": { borderBottom: "unset" } }} // 内部の子全てにボーダーを消すスタイルを適応
+        sx={{
+          "& > *": { borderBottom: "unset" },
+          backgroundColor: backgroundColor,
+        }} // 内部の子全てにボーダーを消すスタイルを適応
       >
         <TableCell
           sx={{
