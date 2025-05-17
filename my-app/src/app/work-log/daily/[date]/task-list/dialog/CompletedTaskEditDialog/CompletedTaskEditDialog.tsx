1d1d3d7d339997cf1fc022aa66b6dd042ef1175b
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-17 12:48:37 +0900

    他のダイアログ繋ぎ込みようにフラグメントで囲い込む

--- my-app/src/app/work-log/daily/[date]/task-list/dialog/CompletedTaskEditDialog/CompletedTaskEditDialog.tsx
+++ my-app/src/app/work-log/daily/[date]/task-list/dialog/CompletedTaskEditDialog/CompletedTaskEditDialog.tsx
@@ -50,86 +50,88 @@ const CompletedTaskEditDialog = memo(function CompletedTaskEditDialog({
       onClose,
     });
   return (
-    <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
-      <DialogContent sx={{ pb: 3 }}>
-        <Stack spacing={3} mt={1}>
-          {/** カテゴリ */}
-          <Stack
-            direction="row"
-            justifyContent={"space-between"}
-            alignItems={"center"}
-            pr={1.5}
-          >
-            <SelectLikeDisplay text={categoryName} width={400} />
-            <AddCircleOutlineIcon color="disabled" />
+    <>
+      <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
+        <DialogContent sx={{ pb: 3 }}>
+          <Stack spacing={3} mt={1}>
+            {/** カテゴリ */}
+            <Stack
+              direction="row"
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              pr={1.5}
+            >
+              <SelectLikeDisplay text={categoryName} width={400} />
+              <AddCircleOutlineIcon color="disabled" />
+            </Stack>
+            {/** タスク */}
+            <Stack
+              direction="row"
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              pr={1.5}
+            >
+              <SelectLikeDisplay text={taskName} width={400} />
+              <AddCircleOutlineIcon color="disabled" />
+            </Stack>
+            {/** 稼働時間/進捗/メモ追加ボタン */}
+            <Stack direction="row" justifyContent={"space-between"} pr={4}>
+              {/** 稼働時間 */}
+              <FormControl sx={{ width: "20%" }}>
+                <InputLabel
+                  id="hours-select-label"
+                  sx={{
+                    left: 0, // ← 左寄せ
+                    transform: "translate(0, -50%) scale(0.75)", // ← 初期位置に強制
+                    whiteSpace: "nowrap", // 改行させない
+                    overflow: "visible", // はみ出ても切らない
+                  }}
+                >
+                  稼働時間(hour)
+                </InputLabel>
+                <Select
+                  labelId="hours-select-label"
+                  id="hours-select"
+                  name="hours-select"
+                  value={String(dailyHours)}
+                  onChange={onChangeSelectHours}
+                  label="稼働時間(hour)"
+                  variant="standard"
+                >
+                  {[...Array(41)].map((_, i) => (
+                    <MenuItem key={i} value={i * 0.25}>
+                      {i * 0.25}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              {/** 進捗 */}
+              <SliderLikeDisplay title={"進捗"} width={250} value={100} />
+              {/** メモ追加ボタン */}
+              <Button startIcon={<AddCommentIcon />}>メモを追加</Button>
+            </Stack>
           </Stack>
-          {/** タスク */}
-          <Stack
-            direction="row"
-            justifyContent={"space-between"}
-            alignItems={"center"}
-            pr={1.5}
-          >
-            <SelectLikeDisplay text={taskName} width={400} />
-            <AddCircleOutlineIcon color="disabled" />
+        </DialogContent>
+        {/** ボタン群 */}
+        <Stack direction="row" justifyContent={"space-between"} py={2} px={4}>
+          <Stack>
+            <Button startIcon={<DeleteIcon />} color="error">
+              削除
+            </Button>
           </Stack>
-          {/** 稼働時間/進捗/メモ追加ボタン */}
-          <Stack direction="row" justifyContent={"space-between"} pr={4}>
-            {/** 稼働時間 */}
-            <FormControl sx={{ width: "20%" }}>
-              <InputLabel
-                id="hours-select-label"
-                sx={{
-                  left: 0, // ← 左寄せ
-                  transform: "translate(0, -50%) scale(0.75)", // ← 初期位置に強制
-                  whiteSpace: "nowrap", // 改行させない
-                  overflow: "visible", // はみ出ても切らない
-                }}
-              >
-                稼働時間(hour)
-              </InputLabel>
-              <Select
-                labelId="hours-select-label"
-                id="hours-select"
-                name="hours-select"
-                value={String(dailyHours)}
-                onChange={onChangeSelectHours}
-                label="稼働時間(hour)"
-                variant="standard"
-              >
-                {[...Array(41)].map((_, i) => (
-                  <MenuItem key={i} value={i * 0.25}>
-                    {i * 0.25}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            {/** 進捗 */}
-            <SliderLikeDisplay title={"進捗"} width={250} value={100} />
-            {/** メモ追加ボタン */}
-            <Button startIcon={<AddCommentIcon />}>メモを追加</Button>
+          <Stack direction="row" spacing={2}>
+            <Button onClick={onClose}>キャンセル</Button>
+            <Button
+              startIcon={<CheckCircleIcon />}
+              variant="contained"
+              onClick={handleSave}
+            >
+              保存
+            </Button>
           </Stack>
         </Stack>
-      </DialogContent>
-      {/** ボタン群 */}
-      <Stack direction="row" justifyContent={"space-between"} py={2} px={4}>
-        <Stack>
-          <Button startIcon={<DeleteIcon />} color="error">
-            削除
-          </Button>
-        </Stack>
-        <Stack direction="row" spacing={2}>
-          <Button onClick={onClose}>キャンセル</Button>
-          <Button
-            startIcon={<CheckCircleIcon />}
-            variant="contained"
-            onClick={handleSave}
-          >
-            保存
-          </Button>
-        </Stack>
-      </Stack>
-    </Dialog>
+      </Dialog>
+    </>
   );
 });
 export default CompletedTaskEditDialog;
