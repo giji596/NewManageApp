c09a46f06bb4cd5b981d33c07c7bd0415b2a084d
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-17 18:33:02 +0900

    編集状態の分岐を追記

--- my-app/src/component/dialog/TagEditDialog/list/TagList.tsx
+++ my-app/src/component/dialog/TagEditDialog/list/TagList.tsx
@@ -1,6 +1,7 @@
 import { TagEditListItem } from "@/type/Tag";
 import { Divider, List, ListItem, Paper } from "@mui/material";
 import { memo } from "react";
+import { TagListLogic } from "./TagListLogic";
 
 type Props = {
   /** タグの一覧 */
@@ -11,12 +12,19 @@ type Props = {
  * タグ編集ダイアログのタグ一覧のリスト
  */
 const TagList = memo(function TagList({ tagList }: Props) {
+  const { isEditTargetId, setEditTarget, clearEditTarget } = TagListLogic();
   return (
     <Paper>
       <List sx={{ p: 0 }}>
         {tagList.map((item, i) => (
           <div key={item.id}>
-            <ListItem>{item.name}</ListItem>
+            <ListItem>
+              {/** TODO:分岐で作成済みのコンポーネントを表示させる */}
+              {/** 編集(ターゲットが自身である場合) */}
+              {isEditTargetId(item.id) && item.name}
+              {/** 表示の場合(ターゲットでない場合) */}
+              {!isEditTargetId(item.id) && item.name}
+            </ListItem>
             {tagList.length - 1 !== i && <Divider />}
           </div>
         ))}
---
+++ my-app/src/component/dialog/TagEditDialog/list/TagListLogic.ts
@@ -0,0 +1,23 @@
+import { useCallback, useState } from "react";
+
+/**
+ * タグ編集ダイアログのタグ一覧のリストのロジック
+ */
+export const TagListLogic = () => {
+  const [editTargetId, setEditTargetId] = useState<number | null>(null); // null=選択なし
+  const isEditTargetId = useCallback(
+    (id: number) => editTargetId === id,
+    [editTargetId]
+  );
+  const setEditTarget = useCallback((id: number) => setEditTargetId(id), []);
+  const clearEditTarget = useCallback(() => setEditTargetId(null), []);
+
+  return {
+    /** 与えられたidが編集対象であるかどうか */
+    isEditTargetId,
+    /** 対象を編集対象にセットする関数 */
+    setEditTarget,
+    /** 編集対象を解除(nullに)する関数 */
+    clearEditTarget,
+  };
+};
