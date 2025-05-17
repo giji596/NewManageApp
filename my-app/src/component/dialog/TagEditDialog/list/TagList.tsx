b068c9c22eba3b0b7c1272d90a67fe09a81c3309
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-17 18:39:44 +0900

    アイテムをリストと繋ぎ込み

--- my-app/src/component/dialog/TagEditDialog/list/TagList.tsx
+++ my-app/src/component/dialog/TagEditDialog/list/TagList.tsx
@@ -2,6 +2,8 @@ import { TagEditListItem } from "@/type/Tag";
 import { Divider, List, ListItem, Paper } from "@mui/material";
 import { memo } from "react";
 import { TagListLogic } from "./TagListLogic";
+import EditTagItem from "./EditTagItem/EditTagItem";
+import DisplayTagItem from "./DisplayTagItem/DisplayTagItem";
 
 type Props = {
   /** タグの一覧 */
@@ -19,11 +21,21 @@ const TagList = memo(function TagList({ tagList }: Props) {
         {tagList.map((item, i) => (
           <div key={item.id}>
             <ListItem>
-              {/** TODO:分岐で作成済みのコンポーネントを表示させる */}
               {/** 編集(ターゲットが自身である場合) */}
-              {isEditTargetId(item.id) && item.name}
+              {isEditTargetId(item.id) && (
+                <EditTagItem
+                  defaultTagName={item.name}
+                  onFinishEdit={clearEditTarget}
+                />
+              )}
               {/** 表示の場合(ターゲットでない場合) */}
-              {!isEditTargetId(item.id) && item.name}
+              {!isEditTargetId(item.id) && (
+                <DisplayTagItem
+                  tagName={item.name}
+                  onClickEdit={() => setEditTarget(item.id)}
+                  onClickDelete={() => {} /** TODO:あとで */}
+                />
+              )}
             </ListItem>
             {tagList.length - 1 !== i && <Divider />}
           </div>
