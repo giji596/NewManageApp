448ccdc7acae3bf6e2b87966d53613766161b312
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-17 17:17:09 +0900

    引数追加

--- my-app/src/component/dialog/TagEditDialog/list/DisplayTagItem/DisplayTagItem.tsx
+++ my-app/src/component/dialog/TagEditDialog/list/DisplayTagItem/DisplayTagItem.tsx
@@ -2,22 +2,35 @@ import { IconButton, ListItemText, Stack } from "@mui/material";
 import { memo } from "react";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
+
+type Props = {
+  /** タグ名 */
+  tagName: string;
+  /** 編集ボタン押した時のハンドラー */
+  onClickEdit: () => void;
+  /** 削除ボタン押した時のハンドラー */
+  onClickDelete: () => void;
+};
 /**
  * タグリストの表示時のアイテム
  */
-const DisplayTagItem = memo(function DisplayTagItem() {
+const DisplayTagItem = memo(function DisplayTagItem({
+  tagName,
+  onClickEdit,
+  onClickDelete,
+}: Props) {
   return (
     <Stack direction="row" justifyContent={"space-between"}>
       {/** 左側(タグ名表示) */}
-      <ListItemText primary={"タグ名" /** TODO:親からもらう */} />
+      <ListItemText primary={tagName} />
       {/** 右側(ボタン部分) */}
       <Stack direction="row">
         {/** 編集ボタン */}
-        <IconButton color="primary">
+        <IconButton color="primary" onClick={onClickEdit}>
           <EditIcon />
         </IconButton>
         {/** 削除ボタン */}
-        <IconButton color="error">
+        <IconButton color="error" onClick={onClickDelete}>
           <DeleteIcon />
         </IconButton>
       </Stack>
