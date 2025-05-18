0620559ffb2330c78449f0832f37a942ee6b5386
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-18 16:13:17 +0900

    ハンドラー定義

--- my-app/src/app/api/work-log/tags/[id]/route.ts
+++ my-app/src/app/api/work-log/tags/[id]/route.ts
@@ -1,5 +1,19 @@
-import { deleteTag } from "@/lib/services/tagService";
+import { deleteTag, updateTagName } from "@/lib/services/tagService";
 import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  // リクエストボディ
+  const { name } = (await req.json()) as { name: string }; // aspidaで型安全にしてるのでasで宣言
+  // パラメータ
+  const { id: idParam } = await params;
+  const id = Number(idParam);
+
+  const res = await updateTagName(name, id);
+  return NextResponse.json(res);
+}
 
 export async function DELETE(
   _: NextRequest,
