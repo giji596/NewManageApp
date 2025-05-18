651f46c205933c909f9a973a5563ecc16b26759b
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-18 17:36:29 +0900

    ボタン/テキストのスタイル・位置調整

--- my-app/src/component/SettingsDrawer/dialog/DataResetDialog/DataResetDialog.tsx
+++ my-app/src/component/SettingsDrawer/dialog/DataResetDialog/DataResetDialog.tsx
@@ -7,6 +7,7 @@ import {
   Typography,
 } from "@mui/material";
 import { memo } from "react";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 /**
  * データリセット時に表示するダイアログ
@@ -18,14 +19,18 @@ const DataResetDialog = memo(function DataResetDialog() {
         <DialogContentText>
           ローカル保存されたデータを削除しますか？
         </DialogContentText>
-        <Typography>
+        <Typography color="textSecondary" pt={3} pl={1}>
           -この操作は、ブラウザに保存されているデータ（IndexedDB）を初期化します。
         </Typography>
-        <Typography>※ インポートしたファイル自体には影響しません。</Typography>
+        <Typography variant="subtitle2" color="textSecondary" pt={2} pl={1}>
+          ※ インポートしたファイル自体には影響しません。
+        </Typography>
       </DialogContent>
       <DialogActions>
         <Button>キャンセル</Button>
-        <Button>リセット</Button>
+        <Button color="error" startIcon={<DeleteForeverIcon />}>
+          削除
+        </Button>
       </DialogActions>
     </Dialog>
   );
