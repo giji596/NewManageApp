525e40fc197897297a658121f42bb466bb72308c
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-20 12:31:06 +0900

    ローカル用のクライアント作成(一ヶ月の稼働記録に関するデータのみ挿入)

---
+++ my-app/src/lib/localClient.ts
@@ -0,0 +1,11 @@
+export const localClient = {
+  work_log: {
+    tasks: {
+      activities: {
+        last_month: {
+          get: () => () => {}, // TODO:get時のロジックをここに
+        },
+      },
+    },
+  },
+};
