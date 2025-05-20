114e242b8380c5eb55ec7f1cc358813e3fce9d94
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-20 15:05:02 +0900

    ロジック置き換え

--- my-app/src/app/work-log/component/pie-chart/MainPagePieChartLogic.ts
+++ my-app/src/app/work-log/component/pie-chart/MainPagePieChartLogic.ts
@@ -1,19 +1,19 @@
-import apiClient from "@/lib/apiClient";
-import useAspidaSWR from "@aspida/swr";
+import { localClient } from "@/lib/localClient";
 import { useRouter } from "next/navigation";
 import { useCallback } from "react";
+import useSWR from "swr";
 
 /**
  * メインページの円グラフのロジック
  */
 export default function MainPagePieChartLogic() {
   const router = useRouter();
-  const { data: rawData } = useAspidaSWR(
-    apiClient.work_log.tasks.activities.last_month,
-    "get",
-    { key: "api/work-log/tasks/activities/last-month" }
+  const { data: rawData } = useSWR(
+    "api/work-log/tasks/activities/last-month",
+    localClient.work_log.tasks.activities.last_month.get()
   );
-  const data = rawData?.body ?? [];
+
+  const data = rawData ?? [];
 
   const navigateCategoryPage = useCallback(
     (id: number) => {
