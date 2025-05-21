f6d67b8475031d74df22cfa12ac185e2bc9369bc
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-21 20:38:12 +0900

    ロジック適応

--- my-app/src/app/work-log/component/table/TaskTableLogic.ts
+++ my-app/src/app/work-log/component/table/TaskTableLogic.ts
@@ -1,20 +1,19 @@
-import apiClient from "@/lib/apiClient";
+import { localClient } from "@/lib/localClient";
 import { MainPageTaskTable } from "@/type/Task";
-import useAspidaSWR from "@aspida/swr";
 import { useRouter } from "next/navigation";
 import { useCallback } from "react";
+import useSWR from "swr";
 
 /**
  * メインページのタスクテーブルコンポーネントのロジック
  */
 export default function TaskTableLogic() {
   const router = useRouter();
-  const { data: rawData, isLoading } = useAspidaSWR(
-    apiClient.work_log.tasks.progress.last_month,
-    "get",
-    { key: "api/work-log/tasks/progress/last-month" }
+  const { data: rawData, isLoading } = useSWR(
+    "api/work-log/tasks/progress/last-month",
+    localClient.work_log.tasks.progress.last_month.get()
   );
-  const data: MainPageTaskTable[] = rawData?.body ?? [];
+  const data: MainPageTaskTable[] = rawData ?? [];
 
   const navigateToDetail = useCallback(
     (id: number) => {
