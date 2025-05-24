9dc7dffb10b80cf11a7ec10393b54932f713092b
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-24 11:11:05 +0900

    ダーク/ライトテーマを定義してThemeProviderでテーマを適応できるように変更

--- my-app/src/app/work-log/layout.tsx
+++ my-app/src/app/work-log/layout.tsx
@@ -1,15 +1,19 @@
+"use client";
+
 import Navbar from "@/component/Navbar/Navbar";
+import { ThemeProvider } from "@mui/material";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import { lightTheme } from "./theme";
 /**
  * work-logページの共通レイアウト
  */
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <ThemeProvider theme={lightTheme}>
       <AppRouterCacheProvider>
         <Navbar />
         <main>{children}</main>
       </AppRouterCacheProvider>
-    </>
+    </ThemeProvider>
   );
 }
