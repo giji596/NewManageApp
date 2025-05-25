a3727cb92542b8200145d73b12a3e8467fa53c9e
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-25 16:22:30 +0900

    ロジック移動

--- my-app/src/app/work-log/daily/[date]/menu/DailyDetailMenuLogic.ts
+++ my-app/src/app/work-log/daily/[date]/menu/DailyDetailMenuLogic.ts
@@ -1,7 +1,10 @@
+import { localClient } from "@/lib/localClient";
 import { TaskLogSummary } from "@/type/Task";
 import { keyframes } from "@emotion/react";
 import { format } from "date-fns";
+import { useParams } from "next/navigation";
 import { useMemo } from "react";
+import useSWR from "swr";
 
 type Props = {
   /** 対象の日付データ */
@@ -15,11 +18,27 @@ type Props = {
 /**
  * 日付詳細　ナビゲーションやらのメニューのコンポーネントのロジック
  */
-export default function DailyDetailMenuLogic({
-  date,
-  dailyHours,
-  taskList,
-}: Props) {
+export default function DailyDetailMenuLogic({}: Props) {
+  const { date: dateParam } = useParams<{ date: string }>();
+  const { data } = useSWR(
+    `api/work-log/daily/${dateParam}`,
+    localClient.work_log.daily._date(dateParam).get()
+  );
+  const date = useMemo(() => {
+    const date = data?.date ?? dateParam;
+    return new Date(date);
+  }, [data?.date, dateParam]);
+  const taskList = useMemo(() => data?.taskList ?? [], [data]);
+  const dailyHours = useMemo(
+    () => taskList.reduce<number>((a, b) => a + b.dailyHours, 0),
+    [taskList]
+  );
+  const taskLogSummary =
+    taskList.reduce<TaskLogSummary[]>((a, b) => {
+      const taskData: TaskLogSummary = { id: b.id, taskName: b.task.name };
+      a.push(taskData);
+      return a;
+    }, []) ?? [];
   // Date => YYYY/MM/DDに変換する
   const dateString = useMemo(() => format(date, "yyyy/MM/dd"), [date]);
 
@@ -34,7 +53,10 @@ export default function DailyDetailMenuLogic({
   }
 `;
   }, [dailyHours]);
-  const isNoTask = useMemo(() => taskList.length === 0, [taskList.length]);
+  const isNoTask = useMemo(
+    () => taskLogSummary.length === 0,
+    [taskLogSummary.length]
+  );
 
   return {
     /** 日付のstring(YYYY/MM/DD) */
