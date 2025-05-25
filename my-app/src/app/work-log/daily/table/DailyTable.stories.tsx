4ac4e0c4223322485b3d9bb7082ba9dfbc180618
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-25 13:56:55 +0900

    storybook修正

--- my-app/src/app/work-log/daily/table/DailyTable.stories.tsx
+++ my-app/src/app/work-log/daily/table/DailyTable.stories.tsx
@@ -1,13 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import DailyTable from "./DailyTable";
-import { DUMMY_DAILY_SUMMARY_DATA } from "@/dummy/daily-page";
 
 const meta = {
   component: DailyTable,
   args: {
-    itemList: DUMMY_DAILY_SUMMARY_DATA,
-    isLoading: false,
     onClickRow: () => {},
   },
 } satisfies Meta<typeof DailyTable>;
@@ -17,5 +14,3 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
-export const Loading: Story = { args: { isLoading: true } };
-export const Empty: Story = { args: { itemList: [] } };
