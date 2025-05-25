6f08b058880ed0275f9f222eb583836afb3e4667
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-25 15:57:33 +0900

    storybookしゅうせい

--- my-app/src/app/work-log/daily/[date]/task-list/TaskList.stories.tsx
+++ my-app/src/app/work-log/daily/[date]/task-list/TaskList.stories.tsx
@@ -1,13 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import TaskList from "./TaskList";
-import { DUMMY_TASK_TABLE_LIST } from "@/dummy/daily-page";
 
 const meta = {
   component: TaskList,
   args: {
-    taskList: DUMMY_TASK_TABLE_LIST,
-    isLoading: false,
     selectedItemId: 1,
     handleClickRow: () => {},
   },
@@ -18,4 +15,3 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
-export const Loading: Story = { args: { isLoading: true } };
