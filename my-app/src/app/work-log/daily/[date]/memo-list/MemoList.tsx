abaffda841bb59dd6eef393f442d59ba77630c3b
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-26 16:46:15 +0900

    からむだけ作成して繋ぎ込み

--- my-app/src/app/work-log/daily/[date]/memo-list/MemoList.tsx
+++ my-app/src/app/work-log/daily/[date]/memo-list/MemoList.tsx
@@ -8,6 +8,10 @@ import CustomMenuWrapper from "@/component/menu/CustomMenuWrapper/CustomMenuWrap
 import CustomMenuCheckBox from "@/component/menu/content/CustomMenuCheckBox/CustomMenuCheckBox";
 import CustomMenuWrapperLogic from "@/component/menu/CustomMenuWrapper/CustomMenuWrapperLogic";
 import MemoListLogic from "./MemoListLogic";
+import CustomTable, {
+  ColumnConfig,
+} from "@/component/table/CustomTable/CustomTable";
+import { MemoDailyTask } from "@/type/Memo";
 
 type Props = {
   /** 選択中のタスクのid(ハイライトように) */
@@ -33,9 +37,30 @@ export default function MemoList({ selectedItemTaskId }: Props) {
     doFilterByFilterList,
     isSelectedTaskRow,
   } = MemoListLogic({ selectedItemTaskId });
+
+  const columnsConfig: ColumnConfig<MemoDailyTask>[] = [
+    { key: "title", title: "タイトル", width: "45%", labelProp: "sortable" },
+    {
+      key: "task.name",
+      title: "タスク名",
+      width: "30%",
+      labelProp: "sortableAndFilterable",
+    },
+    {
+      key: "tagName",
+      title: "タグ",
+      width: "15%",
+      labelProp: "sortableAndFilterable",
+    },
+    { key: "id", title: "", width: "10%", renderCell: () => <></> },
+  ];
   return (
     <>
       <TableContainer sx={{ height: 345 }}>
+        <CustomTable<MemoDailyTask>
+          data={memoItemList}
+          columns={columnsConfig}
+        />
         <Table sx={{ tableLayout: "fixed " }}>
           <CustomTableHeader
             isAsc={isAsc}
