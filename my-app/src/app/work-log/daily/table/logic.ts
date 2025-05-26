c31964d0a3ba9782f7a755d931dd7c1450a0a589
Silver-Ginji <silver2ndginji@gmail.com>
2025-05-26 14:15:21 +0900

    不要なパラメータ削除

--- my-app/src/app/work-log/daily/table/logic.ts
+++ my-app/src/app/work-log/daily/table/logic.ts
@@ -1,9 +1,5 @@
-import useTableFilter from "@/hook/useTableFilter";
-import useTableSort from "@/hook/useTableSort";
 import { getTodayMonth, getTodayYear } from "@/lib/date";
 import { localClient } from "@/lib/localClient";
-import { DateSummary } from "@/type/Date";
-import { TableSortTargetType } from "@/type/Table";
 import { format } from "date-fns";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useCallback, useMemo } from "react";
@@ -47,68 +43,6 @@ export default function DailyTableLogic() {
     []
   );
 
-  // itemリストに存在するタスク一覧
-  const defaultTaskFilterList = useMemo(
-    () =>
-      itemList.reduce((a: Record<string, boolean>, b) => {
-        const taskName = b.taskName;
-        if (!(taskName in a)) {
-          a[taskName] = false;
-        }
-        return a;
-      }, {}),
-    [itemList]
-  );
-
-  // itemリストに存在するカテゴリ一覧
-  const defaultCategoryFilterList = useMemo(
-    () =>
-      itemList.reduce((a: Record<string, boolean>, b) => {
-        const categoryName = b.categoryName;
-        if (!(categoryName in a)) {
-          a[categoryName] = false;
-        }
-        return a;
-      }, {}),
-    [itemList]
-  );
-
-  // ソート関数
-  const getSortTarget = useCallback(
-    (
-      a: DateSummary,
-      b: DateSummary,
-      target: string | null
-    ): { c: TableSortTargetType; d: TableSortTargetType } => {
-      switch (target) {
-        case "メインカテゴリ":
-          return { c: a.categoryName, d: b.categoryName };
-        case "メインタスク":
-          return { c: a.taskName, d: b.taskName };
-        case "合計稼働時間":
-          return { c: a.dailyHours, d: b.dailyHours };
-        case "日付":
-          return { c: a.date, d: b.date };
-        default:
-          return { c: a.date, d: b.date };
-      }
-    },
-    []
-  );
-
-  const { isAsc, isSelected, handleClickSortLabel, doSort } = useTableSort({
-    initialTarget: "日付",
-    getSortTarget,
-  });
-  const {
-    doFilterByFilterList: doFilterByTaskFilterList,
-    ...taskFilterChildProps
-  } = useTableFilter({ initialFilterList: defaultTaskFilterList });
-  const {
-    doFilterByFilterList: doFilterByCategoryFilterList,
-    ...categoryFilterChildProps
-  } = useTableFilter({ initialFilterList: defaultCategoryFilterList });
-
   // idからメモのタイトル一覧を取得する関数
   const getMemoTitleArrayById = useCallback(
     (id: number) => {
@@ -120,22 +54,6 @@ export default function DailyTableLogic() {
       return array;
     },
     [dateToId, itemList]
-  );
-
-  const doFilterByFilterList = useCallback(
-    (item: DateSummary) => {
-      // フィルター結果を変数で保持
-      let result: boolean;
-      // カテゴリーでのフィルター
-      result = doFilterByCategoryFilterList(item.categoryName);
-      // カテゴリーフィルター対象外(trueの場合)ならタスクフィルターを検証
-      if (result) {
-        result = doFilterByTaskFilterList(item.taskName);
-      }
-      // 両方のフィルターでカットされていない場合だけ表示
-      return result;
-    },
-    [doFilterByCategoryFilterList, doFilterByTaskFilterList]
   );
 
   // ページ移動
@@ -157,24 +75,8 @@ export default function DailyTableLogic() {
     isLoading,
     /** dateをホバー時のメモ開封用にid:number型に変換する関数 */
     dateToId,
-    /** dateをナビゲーション用のパラメータstringに変換する関数 */
-    dateToParam,
-    /** 現在昇順かどうか */
-    isAsc,
-    /** 子用のタスクのフィルター関連のプロパティ */
-    taskFilterChildProps,
-    /** 子用のカテゴリーのフィルター関連のプロパティ */
-    categoryFilterChildProps,
-    /** 選択中かどうか調べる関数 */
-    isSelected,
-    /** ソートラベルをクリックした際のハンドラー */
-    handleClickSortLabel,
-    /** ソートする関数 */
-    doSort,
     /** 該当するidのデータのメモのタイトルの配列を取得する関数 */
     getMemoTitleArrayById,
-    /** フィルターリストに応じてフィルターする関数 */
-    doFilterByFilterList,
     /** 指定された詳細ページにナビゲートするハンドラー */
     handleNavigateSelectedDay,
   };
