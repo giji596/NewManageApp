e179755852d87c41a22c88529ed6fa97170ce16e
Silver-Ginji <silver2ndginji@gmail.com>
2025-06-02 12:46:57 +0900

    UI:コンテンツ部分修正

--- my-app/src/app/work-log/daily/[date]/task-list/dialog/TaskEditDialog/ConfirmDeleteIncludeMemosDialog/ConfirmDeleteIncludeMemosDialog.tsx
+++ my-app/src/app/work-log/daily/[date]/task-list/dialog/TaskEditDialog/ConfirmDeleteIncludeMemosDialog/ConfirmDeleteIncludeMemosDialog.tsx
@@ -9,10 +9,19 @@ import { memo } from "react";
 
 const ConfirmDeleteIncludeMemosDialog = memo(
   function ConfirmDeleteIncludeMemosDialog() {
+    const memoTitles = ["メモ1", "メモ2", "メモ3"];
     return (
       <Dialog open={true} onClose={() => {}}>
-        <DialogTitle>メモを含めて削除</DialogTitle>
-        <DialogContent>本当に削除しますか?</DialogContent>
+        <DialogTitle>ログに関連したメモが存在します</DialogTitle>
+        <DialogContent>
+          ログを削除すると以下のメモも削除されます
+          <ul>
+            {memoTitles.map((title, idx) => (
+              <li key={idx}>{title}</li>
+            ))}
+          </ul>
+          本当に削除しますか?
+        </DialogContent>
         <DialogActions>
           <Button onClick={() => {}}>キャンセル</Button>
           <Button onClick={() => {}} color="error">
