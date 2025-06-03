0b4058c47b362b9bd831c328f5b5b7d5e9e4b648
Silver-Ginji <silver2ndginji@gmail.com>
2025-06-03 15:20:40 +0900

    とりあえずrecharts配置

---
+++ my-app/src/app/work-log/category/category-compare-graph/line-graph/CategoryLineGraph.tsx
@@ -0,0 +1,47 @@
+import { memo } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+
+/**
+ * カテゴリのタスク稼働率を折れ線グラフで表現するコンポーネント
+ */
+const CategoryLineGraph = memo(function CategoryLineGraph() {
+  const data = [
+    {
+      date: "2022-01-01",
+      value: 50,
+    },
+    {
+      date: "2022-01-05",
+      value: 40,
+    },
+    {
+      date: "2022-01-10",
+      value: 30,
+    },
+    {
+      date: "2022-01-15",
+      value: 20,
+    },
+    {
+      date: "2022-01-20",
+      value: 10,
+    },
+  ];
+  return (
+    <LineChart width={500} height={350} data={data}>
+      <CartesianGrid stroke="#ccc" />
+      <XAxis dataKey="date" />
+      <YAxis />
+      <Line type="monotone" dataKey="value" stroke="#8884d8" />
+      <Tooltip />
+    </LineChart>
+  );
+});
+export default CategoryLineGraph;
