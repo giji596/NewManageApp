e38df93707c504ffcb18e05bac8f0dc23db5d22c
Silver-Ginji <silver2ndginji@gmail.com>
2025-06-03 17:51:40 +0900

    リストの方の内容を修正

--- my-app/src/app/work-log/category/category-compare-graph/line-graph/setting-menu/CategoryLineGraphSettingMenu.tsx
+++ my-app/src/app/work-log/category/category-compare-graph/line-graph/setting-menu/CategoryLineGraphSettingMenu.tsx
@@ -23,7 +23,40 @@ const CategoryLineGraphSettingMenu = memo(
         <Stack width="30%" height="50vh" border="1px solid">
           <List>
             <ListItem>
-              <ListItemButton>aaa</ListItemButton>
+              <ListItemButton sx={{ padding: 0 }}>
+                {/** チェックのボタンっぽいところ */}
+                <div
+                  style={{
+                    width: 12,
+                    height: 12,
+                    border: "1px solid rgba(26, 26, 26, 0.14)",
+                    borderRadius: "50%", // 丸くする
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginLeft: 8,
+                    marginRight: 8,
+                    backgroundColor: "transparent", // 選択された場合の背景色
+                    transition: "all 0.3s ease", // アニメーションのスムーズさ
+                    boxShadow: true // TODO:選択中かどうかをフラグ管理して表示
+                      ? "0 0 10px rgba(25, 118, 210, 0.6)"
+                      : "none", // 選択時にグラデーションっぽく外側に広がる影
+                  }}
+                >
+                  {true && ( // TODO:選択中かどうかをフラグ管理して表示
+                    <div
+                      style={{
+                        width: 10,
+                        height: 10,
+                        borderRadius: "50%", // 中も丸く
+                        backgroundColor: "rgb(28, 164, 255)", // チェック部分の色 TODO:項目によって変更
+                      }}
+                    />
+                  )}
+                </div>
+                {/** カテゴリ名 */}
+                aaa
+              </ListItemButton>
             </ListItem>
           </List>
         </Stack>
