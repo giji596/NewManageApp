782196462038e5baf23dfe2d3f97047ca3e22c52
Silver-Ginji <silver2ndginji@gmail.com>
2025-06-09 14:19:42 +0900

    データない場合の表示作成

--- my-app/src/app/work-log/category/category-compare-graph/line-graph/CategoryLineGraph.tsx
+++ my-app/src/app/work-log/category/category-compare-graph/line-graph/CategoryLineGraph.tsx
@@ -15,6 +15,7 @@ import {
   CategoryLineGraphRange,
 } from "@/type/Category";
 import { useThemeColor } from "@/hook/useThemeColor";
+import { Stack, Typography } from "@mui/material";
 
 type Props = {
   /** 幅 */
@@ -56,46 +57,53 @@ const CategoryLineGraph = memo(function CategoryLineGraph({
   } = useThemeColor();
   return (
     <>
-      <LineChart width={width} height={350} data={data}>
-        <CartesianGrid stroke="#ccc" />
-        <XAxis
-          dataKey="date"
-          label={{
-            value: xLabel,
-            position: "insideBottomRight",
-            offset: 0,
-            style: { fill: lineGraphAxisTextColor },
-          }}
-          tick={{ fill: lineGraphAxisTextColor }}
-        />
-        <YAxis
-          label={{
-            value: yLabel,
-            angle: -90,
-            position: "insideLeft",
-            style: { fill: lineGraphAxisTextColor },
-          }}
-          tick={{ fill: lineGraphAxisTextColor }}
-        />
-        {dataInfo.map((info) => (
-          <Line
-            key={info.key}
-            dataKey={info.key}
-            name={info.name}
-            stroke={getLineGraphThemeColor(info.color)}
-            strokeWidth={5}
-            style={{ cursor: "pointer" }}
-            onClick={() => onClickLine(info.key)}
-            hide={!visibleKeys[info.name]}
+      {data.length === 0 && (
+        <Stack width={450} height={310} border="1px solid">
+          <Typography>データがありません</Typography>
+        </Stack>
+      )}
+      {data.length > 0 && (
+        <LineChart width={width} height={350} data={data}>
+          <CartesianGrid stroke="#ccc" />
+          <XAxis
+            dataKey="date"
+            label={{
+              value: xLabel,
+              position: "insideBottomRight",
+              offset: 0,
+              style: { fill: lineGraphAxisTextColor },
+            }}
+            tick={{ fill: lineGraphAxisTextColor }}
           />
-        ))}
-        <Tooltip
-          contentStyle={{
-            backgroundColor: tooltipBackGroundColor,
-            color: tooltipTextColor,
-          }}
-        />
-      </LineChart>
+          <YAxis
+            label={{
+              value: yLabel,
+              angle: -90,
+              position: "insideLeft",
+              style: { fill: lineGraphAxisTextColor },
+            }}
+            tick={{ fill: lineGraphAxisTextColor }}
+          />
+          {dataInfo.map((info) => (
+            <Line
+              key={info.key}
+              dataKey={info.key}
+              name={info.name}
+              stroke={getLineGraphThemeColor(info.color)}
+              strokeWidth={5}
+              style={{ cursor: "pointer" }}
+              onClick={() => onClickLine(info.key)}
+              hide={!visibleKeys[info.name]}
+            />
+          ))}
+          <Tooltip
+            contentStyle={{
+              backgroundColor: tooltipBackGroundColor,
+              color: tooltipTextColor,
+            }}
+          />
+        </LineChart>
+      )}
     </>
   );
 });
