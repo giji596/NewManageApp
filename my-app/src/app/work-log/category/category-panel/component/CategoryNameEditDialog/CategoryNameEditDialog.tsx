ff05a5bc833f0f273b66e60006e5f408237ea322
giji596 <giji596@gmail.com>
2025-06-10 09:04:43 +0900

    ボタンの色/Textfieldのラベル設定

--- my-app/src/app/work-log/category/category-panel/component/CategoryNameEditDialog/CategoryNameEditDialog.tsx
+++ my-app/src/app/work-log/category/category-panel/component/CategoryNameEditDialog/CategoryNameEditDialog.tsx
@@ -8,6 +8,7 @@ import {
   Typography,
 } from "@mui/material";
 import { memo } from "react";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 /**
  * カテゴリ名を編集するダイアログ
@@ -18,11 +19,11 @@ const CategoryNameEditDialog = memo(function CategoryNameEditDialog() {
       <DialogTitle>カテゴリ名を編集</DialogTitle>
       <Stack py={0.5} px={6} spacing={1}>
         <Typography>変更前の名称:{/** TODO: */}</Typography>
-        <TextField />
+        <TextField id="category-name" label="変更後の名称" />
       </Stack>
       <DialogActions>
-        <Button>キャンセル</Button>
-        <Button>保存</Button>
+        <Button color="error">キャンセル</Button>
+        <Button startIcon={<CheckCircleIcon />}>保存</Button>
       </DialogActions>
     </Dialog>
   );
